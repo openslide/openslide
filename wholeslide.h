c514c3c5d0c1cc607293f1684d713ba89b5431ba
Adam Goode <agoode@andrew.cmu.edu>
2007-11-26 00:17:40 -0500

    Partial documentation completed

--- wholeslide.h
+++ wholeslide.h
@@ -7,15 +7,46 @@
 
 typedef struct _wholeslide wholeslide_t;
 
-/* one of many possible create functions */
-wholeslide_t *wholeslide_create_from_file(FILE *f);
+/**
+ * Open a whole slide image.
+ *
+ * @param filename The filename to open.
+ * @return A new handle to an open whole slide image.
+ */
+wholeslide_t *ws_open(const char *filename);
 
-/* copy uncompressed RGB data into region */
-/* TODO: different RGB formats? */
-void wholeslide_fill_region(wholeslide_t *ws,
-			    void *region,
-			    double x, double y, double z,
-			    uint32_t w, uint32_t h);
+/**
+ * Compute minimum size for given image region.
+ *
+ * @param ws The whole slide image handle.
+ * @param x The top left x-coordinate.
+ * @param y The top left y-coordinate.
+ * @param slice The desired slice.
+ * @param w The width of the region.
+ * @param h The height of the region.
+ * @return The minimum number of bytes to allocate to hold the uncompressed image data for the region.
+ */
+uint32_t ws_get_region_num_bytes(wholeslide_t *ws,
+				 uint32_t x, uint32_t y,
+				 const char *slice,
+				 uint32_t w, uint32_t h);
+
+/**
+ * Copy RGBA data from a whole slide image.
+ */
+void ws_read_region(wholeslide_t *ws,
+		    void *dest,
+		    uint32_t x, uint32_t y,
+		    const char *slice,
+		    uint32_t w, uint32_t h);
+
+int ws_prefetch_hint(wholeslide_t *ws,
+		     uint32_t x, uint32_t y,
+		     const char *slice,
+		     uint32_t w, uint32_t h);
+
+void ws_cancel_prefetch_hint(wholeslide_t *ws, int prefetch_id);
+
 
 /* query list of preferred zoom factors */
 uint32_t wholeslide_get_preferred_zoom_count(wholeslide_t *ws);
