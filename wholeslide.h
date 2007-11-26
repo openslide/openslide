31850794ce3cf1df92eaa4da75eeb7a5aa70336c
Adam Goode <agoode@andrew.cmu.edu>
2007-11-26 09:47:38 -0500

    More documentation

--- wholeslide.h
+++ wholeslide.h
@@ -33,6 +33,14 @@ uint32_t ws_get_region_num_bytes(wholeslide_t *ws,
 
 /**
  * Copy RGBA data from a whole slide image.
+ *
+ * @param ws The whole slide image handle.
+ * @param dest The destination buffer for the RGBA data.
+ * @param x The top left x-coordinate.
+ * @param y The top left y-coordinate.
+ * @param slice The desired slice.
+ * @param w The width of the region.
+ * @param h The height of the region.
  */
 void ws_read_region(wholeslide_t *ws,
 		    void *dest,
@@ -40,24 +48,37 @@ void ws_read_region(wholeslide_t *ws,
 		    const char *slice,
 		    uint32_t w, uint32_t h);
 
+/**
+ * Give a hint that a region is likely to be needed soon.
+ *
+ * @param ws The whole slide image handle.
+ * @param x The top left x-coordinate.
+ * @param y The top left y-coordinate.
+ * @param slice The desired slice.
+ * @param w The width of the region.
+ * @param h The height of the region.
+ * @returns A unique identifier for this prefetch hint.
+ */
 int ws_prefetch_hint(wholeslide_t *ws,
 		     uint32_t x, uint32_t y,
 		     const char *slice,
 		     uint32_t w, uint32_t h);
 
+/**
+ * Cancel an existing prefetch hint.
+ *
+ * @param ws The whole slide image handle.
+ * @param prefetch_id An identifier returned by ws_prefetch_hint().
+ */
 void ws_cancel_prefetch_hint(wholeslide_t *ws, int prefetch_id);
 
 
-/* query list of preferred zoom factors */
-uint32_t wholeslide_get_preferred_zoom_count(wholeslide_t *ws);
-double wholeslide_get_one_preferred_zoom(wholeslide_t *ws, uint32_t n);
-
-/* destructor */
-void wholeslide_destroy(wholeslide_t *ws);
-
-
-/* TODO: error handling */
-/* TODO: metadata */
+/**
+ * Close a whole slide image handle.
+ *
+ * @param ws The whole slide image handle.
+ */
+void ws_close(wholeslide_t *ws);
 
 
 #endif
