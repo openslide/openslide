76d12550831d26a6bab971c1c275b677e05628fa
Adam Goode <agoode@andrew.cmu.edu>
2007-12-11 23:03:03 -0500

    Rename *ws to *wsd in header file

--- src/wholeslide.h
+++ src/wholeslide.h
@@ -24,7 +24,7 @@ wholeslide_t *ws_open(const char *filename);
 /**
  * Compute minimum size for given image region.
  *
- * @param ws The whole slide image handle.
+ * @param wsd The whole slide image handle.
  * @param x The top left x-coordinate.
  * @param y The top left y-coordinate.
  * @param layer The desired layer.
@@ -32,7 +32,7 @@ wholeslide_t *ws_open(const char *filename);
  * @param h The height of the region.
  * @return The minimum number of bytes needed to hold the uncompressed image data for the region.
  */
-uint32_t ws_get_region_num_bytes(wholeslide_t *ws,
+uint32_t ws_get_region_num_bytes(wholeslide_t *wsd,
 				 uint32_t x, uint32_t y,
 				 uint32_t layer,
 				 uint32_t w, uint32_t h);
@@ -45,7 +45,7 @@ uint32_t ws_get_region_num_bytes(wholeslide_t *ws,
  * pointer to enough memory to hold the region. To compute the proper
  * size for @p dest, use ws_get_region_num_bytes().
  *
- * @param ws The whole slide image handle.
+ * @param wsd The whole slide image handle.
  * @param dest The destination buffer for the RGBA data.
  * @param x The top left x-coordinate.
  * @param y The top left y-coordinate.
@@ -53,7 +53,7 @@ uint32_t ws_get_region_num_bytes(wholeslide_t *ws,
  * @param w The width of the region.
  * @param h The height of the region.
  */
-void ws_read_region(wholeslide_t *ws,
+void ws_read_region(wholeslide_t *wsd,
 		    void *dest,
 		    uint32_t x, uint32_t y,
 		    uint32_t layer,
@@ -62,7 +62,7 @@ void ws_read_region(wholeslide_t *ws,
 /**
  * Give a hint that a region is likely to be needed soon.
  *
- * @param ws The whole slide image handle.
+ * @param wsd The whole slide image handle.
  * @param x The top left x-coordinate.
  * @param y The top left y-coordinate.
  * @param layer The desired layer.
@@ -70,7 +70,7 @@ void ws_read_region(wholeslide_t *ws,
  * @param h The height of the region.
  * @returns A unique identifier for this prefetch hint.
  */
-int ws_give_prefetch_hint(wholeslide_t *ws,
+int ws_give_prefetch_hint(wholeslide_t *wsd,
 			  uint32_t x, uint32_t y,
 			  uint32_t layer,
 			  uint32_t w, uint32_t h);
@@ -78,73 +78,73 @@ int ws_give_prefetch_hint(wholeslide_t *ws,
 /**
  * Cancel an existing prefetch hint.
  *
- * @param ws The whole slide image handle.
+ * @param wsd The whole slide image handle.
  * @param prefetch_id An identifier returned by ws_give_prefetch_hint().
  */
-void ws_cancel_prefetch_hint(wholeslide_t *ws, int prefetch_id);
+void ws_cancel_prefetch_hint(wholeslide_t *wsd, int prefetch_id);
 
 
 /**
  * Close a whole slide image handle.
  *
- * @param ws The whole slide image handle.
+ * @param wsd The whole slide image handle.
  */
-void ws_close(wholeslide_t *ws);
+void ws_close(wholeslide_t *wsd);
 
 /**
  * Get the number of layers in the whole slide image.
  *
- * @param ws The whole slide image handle.
+ * @param wsd The whole slide image handle.
  * @return The number of layers.
  */
-uint32_t ws_get_layer_count(wholeslide_t *ws);
+uint32_t ws_get_layer_count(wholeslide_t *wsd);
 
 /**
  * Get the dimensions of the baseline image.
  *
- * @param ws The whole slide image handle.
+ * @param wsd The whole slide image handle.
  * @param[out] w The width of the image.
  * @param[out] h The height of the image.
  */
-void ws_get_baseline_dimensions(wholeslide_t *ws, uint32_t *w, uint32_t *h);
+void ws_get_baseline_dimensions(wholeslide_t *wsd, uint32_t *w, uint32_t *h);
 
 /**
  * Get the dimensions of a layer.
  *
- * @param ws The whole slide image handle.
+ * @param wsd The whole slide image handle.
  * @param layer The desired layer.
  * @param[out] w The width of the image.
  * @param[out] h The height of the image.
  */
-void ws_get_layer_dimensions(wholeslide_t *ws, uint32_t layer,
+void ws_get_layer_dimensions(wholeslide_t *wsd, uint32_t layer,
 			     uint32_t *w, uint32_t *h);
 
 /**
  * Get the downsampling factor of a given layer.
  *
- * @param ws The whole slide image handle.
+ * @param wsd The whole slide image handle.
  * @param layer The desired layer.
  * @return The downsampling factor for this layer.
  */
-double ws_get_layer_downsample(wholeslide_t *ws, uint32_t layer);
+double ws_get_layer_downsample(wholeslide_t *wsd, uint32_t layer);
 
 
 /**
- * Get the next bigger layer for the given downsample.
+ * Get the best layer to use for displaying the given downsample.
  *
- * @param ws The whole slide image handle.
+ * @param wsd The whole slide image handle.
  * @param downsample The downsample factor.
  * @return The layer identifier.
  */
-uint32_t ws_get_next_bigger_layer(wholeslide_t *ws, double downsample);
+uint32_t ws_get_best_layer_for_downsample(wholeslide_t *wsd, double downsample);
 
 
 /**
  * Get the comment (if any) for this image.
  *
- * @param ws The whole slide image handle.
+ * @param wsd The whole slide image handle.
  * @return The comment for this image.
  */
-const char *ws_get_comment(wholeslide_t *ws);
+const char *ws_get_comment(wholeslide_t *wsd);
 
 #endif
