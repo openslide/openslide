1f845594a886309824a72846056339a2e30ff295
Adam Goode <agoode@andrew.cmu.edu>
2007-12-12 00:38:30 -0500

    Test read region

--- src/test.c
+++ src/test.c
@@ -1,9 +1,50 @@
+#define _GNU_SOURCE
+
 #include "wholeslide.h"
+
+#include <stdio.h>
+#include <stdlib.h>
 
 static void test_next_biggest(wholeslide_t *wsd, double downsample) {
   uint32_t layer = ws_get_best_layer_for_downsample(wsd, downsample);
   printf("layer for downsample %g: %d (%g)\n",
 	 downsample, layer, ws_get_layer_downsample(wsd, layer));
+}
+
+static void test_image_fetch(wholeslide_t *wsd,
+			     const char *name,
+			     uint32_t x, uint32_t y,
+			     uint32_t w, uint32_t h) {
+  char *filename;
+
+  for (uint32_t layer = 0; layer < ws_get_layer_count(wsd); layer++) {
+    asprintf(&filename, "%s-%.2d.ppm", name, layer);
+    FILE *f = fopen(filename, "w");
+    if (f == NULL) {
+      perror("Cannot open file");
+      goto end;
+    }
+
+    size_t num_bytes = ws_get_region_num_bytes(wsd, w, h);
+    printf("Going to allocate %d bytes...\n", num_bytes);
+    uint8_t *buf = malloc(num_bytes);
+
+    ws_read_region(wsd, buf, x, y, layer, w, h);
+
+    // write as PPM
+    fprintf(f, "P6\n%d %d\n255\n", w, h);
+    for (int i = 0; i < num_bytes; i+=4) {
+      putc(buf[i + 0], f); // R
+      putc(buf[i + 1], f); // G
+      putc(buf[i + 2], f); // B
+      // no A
+    }
+
+    free(buf);
+    fclose(f);
+  end:
+    free(filename);
+  }
 }
 
 int main(int argc, char **argv) {
@@ -39,6 +80,8 @@ int main(int argc, char **argv) {
   uint32_t prefetch_hint = ws_give_prefetch_hint(wsd, 0, 0, 0, 5, 5);
   ws_cancel_prefetch_hint(wsd, prefetch_hint);
 
+
+  test_image_fetch(wsd, "test1", w / 2, h / 2, 1000, 1000);
 
   ws_close(wsd);
 
