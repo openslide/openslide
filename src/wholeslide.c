e0f9d1a3f134968af78b9932bfb3ba597d7688b9
Adam Goode <agoode@andrew.cmu.edu>
2007-12-13 00:28:44 -0500

    Getting closer on single tile drawing

--- src/wholeslide.c
+++ src/wholeslide.c
@@ -55,7 +55,7 @@ static void parse_trestle(wholeslide_t *wsd) {
   wsd->downsamples = g_new(double, wsd->layer_count);
 
   // directories are linear
-  for (unsigned int i = 0; i < wsd->layer_count; i++) {
+  for (uint32_t i = 0; i < wsd->layer_count; i++) {
     wsd->layers[i] = i;
   }
 
@@ -65,7 +65,7 @@ static void parse_trestle(wholeslide_t *wsd) {
 
   // compute downsamples
   printf("downsamples\n");
-  for (unsigned int i = 0; i < wsd->layer_count; i++) {
+  for (uint32_t i = 0; i < wsd->layer_count; i++) {
     uint32_t w, h;
     ws_get_layer_dimensions(wsd, i, &w, &h);
 
@@ -96,7 +96,7 @@ static bool extract_info(wholeslide_t *wsd) {
 }
 
 static void print_info(wholeslide_t *wsd) {
-  unsigned int i;
+  uint32_t i;
 
   printf("layers: \n");
   for (i = 0; i < wsd->layer_count; i++) {
@@ -208,7 +208,7 @@ uint32_t ws_get_best_layer_for_downsample(wholeslide_t *wsd,
   }
 
   // find where we are in the middle
-  for (unsigned int i = 1; i < wsd->layer_count; i++) {
+  for (uint32_t i = 1; i < wsd->layer_count; i++) {
     if (downsample < wsd->downsamples[i]) {
       return i - 1;
     }
@@ -248,13 +248,68 @@ size_t ws_get_region_num_bytes(wholeslide_t *wsd,
 }
 
 
+static void copy_rgba_tile(uint32_t *tile,
+			   uint8_t *dest,
+			   uint32_t src_w, uint32_t src_h,
+			   int32_t dest_origin_x, int32_t dest_origin_y,
+			   uint32_t dest_w, uint32_t dest_h) {
+  uint32_t src_origin_y;
+  if (dest_origin_y < 0) {  // off the top
+    src_origin_y = src_h - 1 + dest_origin_y;
+  } else {
+    src_origin_y = src_h - 1;
+  }
+
+  printf("src_origin_y: %d, dest_origin_y: %d\n",
+	 src_origin_y, dest_origin_y);
+
+  uint32_t src_origin_x;
+  if (dest_origin_x < 0) {  // off the left
+    src_origin_x = -dest_origin_x;
+  } else {
+    src_origin_x = 0;
+  }
+
+  printf("src_origin_x: %d, dest_origin_x: %d\n",
+	 src_origin_x, dest_origin_x);
+
+
+  for (uint32_t src_y = src_origin_y; (int32_t) src_y >= 0; src_y--) {
+    int32_t dest_y = dest_origin_y + src_h - src_y;  // inverted y
+    printf("src_y: %d, dest_y: %d\n", src_y, dest_y);
+    if (dest_y >= dest_h) {
+      break;
+    }
+
+    for (uint32_t src_x = src_origin_x; src_x < src_w; src_x++) {
+      int32_t dest_x = dest_origin_x + src_x;
+      if (dest_x >= dest_w) {
+	break;
+      }
+
+      uint32_t dest_i = (dest_y * dest_w + dest_x) * 4;
+      uint32_t i = src_y * src_w + src_x;
+
+      //      printf("%d %d -> %d %d\n", x, y, dest_x, dest_y);
+
+      if (TIFFGetA(tile[i])) {
+	  dest[dest_i + 0] = TIFFGetR(tile[i]);
+	  dest[dest_i + 1] = TIFFGetG(tile[i]);
+	  dest[dest_i + 2] = TIFFGetB(tile[i]);
+	  dest[dest_i + 3] = TIFFGetA(tile[i]);
+      }
+    }
+  }
+}
+
+
 void ws_read_region(wholeslide_t *wsd,
 		    void *dest,
 		    uint32_t x, uint32_t y,
 		    uint32_t layer,
 		    uint32_t w, uint32_t h) {
   // fill with background, for now
-  for (unsigned int i = 0; i < w * h * 4; i += 4) {
+  for (uint32_t i = 0; i < w * h * 4; i += 4) {
     ((uint8_t *) dest)[i + 0] = (wsd->background_color >> 24) & 0xFF;
     ((uint8_t *) dest)[i + 1] = (wsd->background_color >> 16) & 0xFF;
     ((uint8_t *) dest)[i + 2] = (wsd->background_color >> 8) & 0xFF;
@@ -266,26 +321,30 @@ void ws_read_region(wholeslide_t *wsd,
     return;
   }
 
+  double downsample = ws_get_layer_downsample(wsd, layer);
+  uint32_t ds_x = x / downsample;
+  uint32_t ds_y = y / downsample;
+
   TIFFSetDirectory(wsd->tiff, wsd->layers[layer]);
 
-  // allocate space for tile
+  // allocate space for 1 tile
   uint32_t tw, th;
   TIFFGetField(wsd->tiff, TIFFTAG_TILEWIDTH, &tw);
   TIFFGetField(wsd->tiff, TIFFTAG_TILELENGTH, &th);
   uint32_t *tile = malloc(tw * th * sizeof(uint32_t));
 
+  // figure out range of tiles
+
+  // for each tile, draw it where it should go
+
   // XXX for now
-  TIFFReadRGBATile(wsd->tiff, 0, 0, tile);
-  for (unsigned int y = 0; y < th; y++) {
-    for (unsigned int x = 0; x < tw; x++) {
-      unsigned int i = y * tw + x;
-      unsigned int dest_i = y * w + x;
-      ((uint8_t *) dest)[4 * dest_i + 0] = TIFFGetR(tile[i]);
-      ((uint8_t *) dest)[4 * dest_i + 1] = TIFFGetG(tile[i]);
-      ((uint8_t *) dest)[4 * dest_i + 2] = TIFFGetB(tile[i]);
-      ((uint8_t *) dest)[4 * dest_i + 3] = TIFFGetA(tile[i]);
-    }
-  }
+  uint32_t round_x = (ds_x / tw) * tw;
+  uint32_t round_y = (ds_y / th) * th;
+  uint32_t off_x = ds_x - round_x;
+  uint32_t off_y = ds_y - round_y;
+
+  TIFFReadRGBATile(wsd->tiff, round_x, round_y, tile);
+  copy_rgba_tile(tile, dest, tw, th, -off_x, -off_y, w, h);
 
   free(tile);
 }
