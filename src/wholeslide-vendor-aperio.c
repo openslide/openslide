7f132d03e9e310489bc4d3cd85d1d9f5d08d5d7d
Adam Goode <agoode@andrew.cmu.edu>
2008-01-22 17:27:04 -0500

    Aperio close to done ?

--- src/wholeslide-vendor-aperio.c
+++ src/wholeslide-vendor-aperio.c
@@ -1,5 +1,7 @@
 #include "config.h"
 
+#include <jasper/jasper.h>
+
 #include "wholeslide-private.h"
 
 #include <glib.h>
@@ -8,6 +10,63 @@
 #include <tiffio.h>
 
 static const char APERIO_DESCRIPTION[] = "Aperio Image Library";
+
+struct _ws_tiff_tilereader {
+  TIFF *tiff;
+};
+
+static struct _ws_tiff_tilereader *_ws_aperio_tiff_tilereader_create(TIFF *tiff) {
+  struct _ws_tiff_tilereader *wtt = g_slice_new(struct _ws_tiff_tilereader);
+
+  wtt->tiff = tiff;
+  return wtt;
+}
+
+static void _ws_aperio_tiff_tilereader_read(struct _ws_tiff_tilereader *wtt,
+					    uint32_t *dest,
+					    uint32_t x, uint32_t y) {
+  // get tile number
+  ttile_t tile_no = TIFFComputeTile(wtt->tiff, x, y, 0, 0);
+
+  // get tile size
+  tsize_t max_tile_size = TIFFTileSize(wtt->tiff);
+
+  // get raw tile
+  tdata_t buf = g_slice_alloc(max_tile_size);
+  tsize_t size = TIFFReadRawTile(wtt->tiff, tile_no, buf, max_tile_size); // XXX?
+
+  // make a jasper stream
+  jas_stream_t *jas_stream = jas_stream_memopen(buf, size);
+
+  // make a jasper image
+  jas_image_t *jas_image = jpc_decode(jas_stream, "");
+
+  // set proper colorspace
+  jas_image_setclrspc(jas_image, JAS_CLRSPC_SYCBCR);
+  jas_image_setcmpttype(jas_image, 0,
+			JAS_IMAGE_CT_COLOR(JAS_CHANIND_YCBCR_Y));
+  jas_image_setcmpttype(jas_image, 1,
+			JAS_IMAGE_CT_COLOR(JAS_CHANIND_YCBCR_CB));
+  jas_image_setcmpttype(jas_image, 2,
+			JAS_IMAGE_CT_COLOR(JAS_CHANIND_YCBCR_CR));
+
+  // set subsampling
+  
+
+  // decode it
+  
+
+  // erase
+  jas_image_destroy(jas_image);
+  jas_stream_close(jas_stream);
+  g_slice_free1(max_tile_size, buf);
+}
+
+static void _ws_aperio_tiff_tilereader_destroy(struct _ws_tiff_tilereader *wtt) {
+  g_slice_free(struct _ws_tiff_tilereader, wtt);
+}
+
+
 
 bool _ws_try_aperio(wholeslide_t *wsd, const char *filename) {
   char *tagval;
@@ -46,10 +105,23 @@ bool _ws_try_aperio(wholeslide_t *wsd, const char *filename) {
   } while (i < layer_count);
 
   // all set, load up the TIFF-specific ops
-  _ws_add_tiff_ops(wsd, tiff, 0, NULL, layer_count, layers,
- 		   _ws_generic_tiff_tilereader_create, /* XXX */
-		   _ws_generic_tiff_tilereader_read,
-		   _ws_generic_tiff_tilereader_destroy);
+  TIFFSetDirectory(tiff, 0);
+  uint16_t compression_mode;
+  TIFFGetField(tiff, TIFFTAG_COMPRESSION, &compression_mode);
+
+  if (compression_mode == 33003) {
+    // special jpeg 2000 aperio thing
+    _ws_add_tiff_ops(wsd, tiff, 0, NULL, layer_count, layers,
+		     _ws_aperio_tiff_tilereader_create,
+		     _ws_aperio_tiff_tilereader_read,
+		     _ws_aperio_tiff_tilereader_destroy);
+  } else {
+    // let libtiff handle it
+    _ws_add_tiff_ops(wsd, tiff, 0, NULL, layer_count, layers,
+		     _ws_generic_tiff_tilereader_create,
+		     _ws_generic_tiff_tilereader_read,
+		     _ws_generic_tiff_tilereader_destroy);
+  }
 
   return true;
 }
