7be932058e9cc873c53632a32be2bd2c16697909
Adam Goode <agoode@andrew.cmu.edu>
2008-10-12 22:45:47 -0400

    First go at working aperio! (B&W for now)

--- src/wholeslide-vendor-aperio.c
+++ src/wholeslide-vendor-aperio.c
@@ -15,10 +15,77 @@ struct _ws_tiff_tilereader {
   TIFF *tiff;
   uint32_t tile_width;
   uint32_t tile_height;
-
-  opj_dinfo_t *decoder;
 };
 
+// OpenJPEG memory-read functions
+struct _ws_opj_mem_stream_state {
+  const tdata_t buf;
+  const tsize_t size;
+  toff_t offset;
+};
+
+static OPJ_UINT32 _ws_opj_mem_stream_read (void *p_buffer,
+					   OPJ_UINT32 p_nb_bytes,
+					   void *p_user_data) {
+  struct _ws_opj_mem_stream_state *ss =
+    (struct _ws_opj_mem_stream_state *) p_user_data;
+
+  //  printf("READ: %p, %d\n", p_buffer, p_nb_bytes);
+
+  if (ss->offset == ss->size) {
+    return -1; // EOF
+  }
+
+  toff_t new_offset = ss->offset + p_nb_bytes;
+  size_t bytes_to_read = p_nb_bytes;
+
+  if (new_offset > ss->size) {
+    toff_t over = new_offset - ss->size;
+    bytes_to_read -= over;
+    new_offset = ss->size;
+  }
+
+  memcpy(p_buffer, ss->buf + ss->offset, bytes_to_read);
+  ss->offset = new_offset;
+
+  return bytes_to_read;
+}
+
+static OPJ_SIZE_T _ws_opj_mem_stream_skip (OPJ_SIZE_T p_nb_bytes,
+					   void *p_user_data) {
+  struct _ws_opj_mem_stream_state *ss =
+    (struct _ws_opj_mem_stream_state *) p_user_data;
+
+  if (ss->offset == ss->size) {
+    return -1; // EOF
+  }
+  toff_t new_offset = ss->offset + p_nb_bytes;
+  size_t bytes_to_skip = p_nb_bytes;
+
+  if (new_offset > ss->size) {
+    toff_t over = new_offset - ss->size;
+    bytes_to_skip -= over;
+    new_offset = ss->size;
+  }
+
+  ss->offset = new_offset;
+
+  return bytes_to_skip;
+}
+
+static bool _ws_opj_mem_stream_seek (OPJ_SIZE_T p_nb_bytes,
+				     void *p_user_data) {
+  struct _ws_opj_mem_stream_state *ss =
+    (struct _ws_opj_mem_stream_state *) p_user_data;
+
+  if (p_nb_bytes > ss->size) {
+    return false;
+  }
+
+  ss->offset = p_nb_bytes;
+
+  return true;
+}
 
 // XXX revisit assumptions that color is always downsampled in x by 2
 static struct _ws_tiff_tilereader *_ws_aperio_tiff_tilereader_create(TIFF *tiff) {
@@ -34,9 +101,6 @@ static struct _ws_tiff_tilereader *_ws_aperio_tiff_tilereader_create(TIFF *tiff)
   wtt->tile_width = w;
   wtt->tile_height = h;
 
-  // make a decoder
-  wtt->decoder = opj_create_decompress(CODEC_J2K);
-
   return wtt;
 }
 
@@ -46,7 +110,7 @@ static void _ws_aperio_tiff_tilereader_read(struct _ws_tiff_tilereader *wtt,
   // get tile number
   ttile_t tile_no = TIFFComputeTile(wtt->tiff, x, y, 0, 0);
 
-  printf("aperio reading tile_no: %d\n", tile_no);
+  //  printf("aperio reading tile_no: %d\n", tile_no);
 
   // get tile size
   tsize_t max_tile_size = TIFFTileSize(wtt->tiff);
@@ -55,19 +119,49 @@ static void _ws_aperio_tiff_tilereader_read(struct _ws_tiff_tilereader *wtt,
   tdata_t buf = g_slice_alloc(max_tile_size);
   tsize_t size = TIFFReadRawTile(wtt->tiff, tile_no, buf, max_tile_size); // XXX?
 
-  // source of compressed data
-  opj_cio_t *j2k_source = opj_cio_open((opj_common_ptr) wtt->decoder, buf, size);
+  // set source of compressed data
+  struct _ws_opj_mem_stream_state stream_state = {
+    .buf = buf,
+    .size = size,
+    .offset = 0,
+  };
+  opj_stream_t *stream = opj_stream_default_create(true);
+  opj_stream_set_read_function(stream, _ws_opj_mem_stream_read);
+  opj_stream_set_skip_function(stream, _ws_opj_mem_stream_skip);
+  opj_stream_set_seek_function(stream, _ws_opj_mem_stream_seek);
+  opj_stream_set_user_data(stream, &stream_state);
 
   // decode
-  opj_image_t *image = opj_decode(wtt->decoder, j2k_source);
+  OPJ_INT32 tx0;
+  OPJ_INT32 ty0;
+  OPJ_UINT32 tw;
+  OPJ_UINT32 th;
+  OPJ_UINT32 ntx;
+  OPJ_UINT32 nty;
+  opj_codec_t *codec = opj_create_decompress(CODEC_J2K);
+  opj_image_t *image;
+  opj_read_header(codec, &image,
+		  &tx0, &ty0, &tw, &th, &ntx, &nty, stream);
+  image = opj_decode(codec, stream);
+  opj_end_decompress(codec, stream);
+  opj_image_comp_t *comps = image->comps;
+
+  // copy
+  for (int i = 0; i < wtt->tile_height * wtt->tile_width; i++) {
+    uint32_t pixel = 255 << 24 | // A
+      (comps[0].data[i] << 16) | // R
+      (comps[0].data[i] << 8) | // G
+      comps[0].data[i]; // B
+    dest[i] = pixel;
+  }
 
   // erase
-  opj_cio_close(j2k_source);
   g_slice_free1(max_tile_size, buf);
+  opj_stream_destroy(stream);
+  opj_destroy_codec(codec);
 }
 
 static void _ws_aperio_tiff_tilereader_destroy(struct _ws_tiff_tilereader *wtt) {
-  opj_destroy_decompress(wtt->decoder);
   g_slice_free(struct _ws_tiff_tilereader, wtt);
 }
 
