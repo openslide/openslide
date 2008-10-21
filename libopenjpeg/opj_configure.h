78bb349421ac5992e5e7ede3e81454537394f199
Adam Goode <agoode@andrew.cmu.edu>
2008-10-21 18:56:28 -0400

    Fix distcheck problems

--- libopenjpeg/opj_configure.h
+++ libopenjpeg/opj_configure.h
@@ -6,8 +6,7 @@
 /* what byte order */
 #ifndef __OPJ_CONFIGURE_H
 #define __OPJ_CONFIGURE_H
-/* #undef CMAKE_WORDS_BIGENDIAN */
-#ifdef CMAKE_WORDS_BIGENDIAN
+#ifdef WORDS_BIGENDIAN
         #define OPJ_BIG_ENDIAN 
 #else
         #define OPJ_LITTLE_ENDIAN 
