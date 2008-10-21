a7e741a8f10ed642fe9c96991e27108382d510b6
Adam Goode <agoode@andrew.cmu.edu>
2008-10-21 19:02:32 -0400

    Add copyright

--- src/wholeslide-ops-jp2k.c
+++ src/wholeslide-ops-jp2k.c
@@ -1,3 +1,27 @@
+/*
+ *  Wholeslide, a library for reading whole slide image files
+ *
+ *  Copyright (c) 2007-2008 Carnegie Mellon University
+ *  All rights reserved.
+ *
+ *  Wholeslide is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 2.
+ *
+ *  Wholeslide is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Wholeslide. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Linking Wholeslide statically or dynamically with other modules is
+ *  making a combined work based on Wholeslide. Thus, the terms and
+ *  conditions of the GNU General Public License cover the whole
+ *  combination.
+ */
+
 #include "config.h"
 
 #define USE_OPJ_DEPRECATED
