afddc5aeb2c9a28a4bf7314ee50ca8775494268a
Adam Goode <agoode@andrew.cmu.edu>
2009-06-22 15:30:32 -0400

    Update stitching script to ints again

--- misc/decode-mirax-stitching.py
+++ misc/decode-mirax-stitching.py
@@ -9,10 +9,8 @@ HEADER_OFFSET = 296
 f.seek(HEADER_OFFSET)
 
 while True:
-    x1 = struct.unpack("<h", f.read(2))[0]
-    x2 = struct.unpack("<h", f.read(2))[0]
-    y1 = struct.unpack("<h", f.read(2))[0]
-    y2 = struct.unpack("<h", f.read(2))[0]
+    x = struct.unpack("<i", f.read(4))[0]
+    y = struct.unpack("<i", f.read(4))[0]
     zz = f.read(1)
 
-    print '%10s %10s %10s %10s %s' % (x1, x2, y1, y2, zz)
+    print '%10s %10s' % (x, y)
