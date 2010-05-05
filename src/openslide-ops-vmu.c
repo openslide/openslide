70f51f8e6e7b806513c3df5f49228c28d6808e3d
Adam Goode <agoode@andrew.cmu.edu>
2010-05-05 11:20:15 -0400

    Fix up whitespace in VMU

--- src/openslide-ops-vmu.c
+++ src/openslide-ops-vmu.c
@@ -39,179 +39,172 @@
 #define MAX_MMAP ( ( int64_t ) SIZE_MAX / 2 )
 
 struct _openslide_vmuopsdata {
-    
-    GMutex *vmu_mutex;
-    
-    int32_t file_count;
-    struct _openslide_vmu_file **files;
-    
+
+  GMutex *vmu_mutex;
+
+  int32_t file_count;
+  struct _openslide_vmu_file **files;
+
 };
 
 
 static void destroy_data(struct _openslide_vmuopsdata *data) {
-    g_mutex_free(data->vmu_mutex);
-    for ( int i = 0; i < data->file_count; i++ ) {
+  g_mutex_free(data->vmu_mutex);
+  for (int i = 0; i < data->file_count; i++) {
 
-	g_free( data->files[i]->filename );
-	g_free( data->files[i]->chunk_table[0] );
-	g_free( data->files[i]->chunk_table );
-	g_free( data->files[i] );
+    g_free(data->files[i]->filename);
+    g_free(data->files[i]->chunk_table[0]);
+    g_free(data->files[i]->chunk_table);
+    g_free(data->files[i]);
 
-    }
-    g_slice_free(struct _openslide_vmuopsdata, data);
+  }
+  g_slice_free(struct _openslide_vmuopsdata, data);
 }
 
-static void destroy(openslide_t *osr) {
-    struct _openslide_vmuopsdata *data = osr->data;
-    destroy_data(data);
+static void destroy(openslide_t * osr) {
+  struct _openslide_vmuopsdata *data = osr->data;
+  destroy_data(data);
 }
 
 
-static void get_dimensions_unlocked(openslide_t *osr, int32_t layer,
-				    int64_t *w, int64_t *h) {
-    
-    struct _openslide_vmuopsdata *data = osr->data;
-    struct _openslide_vmu_file *vmu_file = data->files[layer];
-    
-    *w = vmu_file->w;
-    *h = vmu_file->h;
+static void get_dimensions_unlocked(openslide_t * osr, int32_t layer,
+				    int64_t * w, int64_t * h) {
+
+  struct _openslide_vmuopsdata *data = osr->data;
+  struct _openslide_vmu_file *vmu_file = data->files[layer];
+
+  *w = vmu_file->w;
+  *h = vmu_file->h;
 
 }
 
-static void get_dimensions(openslide_t *osr, int32_t layer,
-			   int64_t *w, int64_t *h) {
-    struct _openslide_vmuopsdata *data = osr->data;
-    
-    g_mutex_lock(data->vmu_mutex);
-    get_dimensions_unlocked(osr, layer, w, h);
-    g_mutex_unlock(data->vmu_mutex);
+static void get_dimensions(openslide_t * osr, int32_t layer,
+			   int64_t * w, int64_t * h) {
+  struct _openslide_vmuopsdata *data = osr->data;
+
+  g_mutex_lock(data->vmu_mutex);
+  get_dimensions_unlocked(osr, layer, w, h);
+  g_mutex_unlock(data->vmu_mutex);
 }
 
-static void paint_region_unlocked( openslide_t *osr, cairo_t *cr,
-				   int64_t x, int64_t y,
-				   int32_t layer,
-				   int32_t w, int32_t h )
-
+static void paint_region_unlocked(openslide_t * osr, cairo_t * cr,
+				  int64_t x, int64_t y,
+				  int32_t layer, int32_t w, int32_t h)
 {
 
-    struct _openslide_vmuopsdata *data = osr->data;
-    struct _openslide_vmu_file *vmu_file = data->files[layer];
-    int64_t **chunk_table = vmu_file->chunk_table;
-    
-    FILE *f = fopen( vmu_file->filename, "rb" );
+  struct _openslide_vmuopsdata *data = osr->data;
+  struct _openslide_vmu_file *vmu_file = data->files[layer];
+  int64_t **chunk_table = vmu_file->chunk_table;
 
-    // Get the initial offset.
+  FILE *f = fopen(vmu_file->filename, "rb");
 
-    int64_t xc = x / vmu_file->chunksize;
-    int64_t offset = chunk_table[y][xc];
+  // Get the initial offset.
 
-    unsigned char *imagedata = 
-	g_slice_alloc( ( int64_t ) w * ( int64_t ) h * ( int64_t ) 4 );
-    memset( imagedata, 0x00, ( int64_t ) w * ( int64_t ) h * ( int64_t ) 4 );
-    
-    int64_t pixelbytes = 3 * sizeof( unsigned short );
-    int64_t chunkbytes = vmu_file->chunksize * pixelbytes;
-    unsigned short *buffer = 
-	( unsigned short *) g_slice_alloc( chunkbytes );
+  int64_t xc = x / vmu_file->chunksize;
+  int64_t offset = chunk_table[y][xc];
 
-    int64_t ct = -1;
-    for ( int64_t j = y, J = 0; J < h; j++, J++ ) {
+  unsigned char *imagedata =
+    g_slice_alloc((int64_t) w * (int64_t) h * (int64_t) 4);
+  memset(imagedata, 0x00, (int64_t) w * (int64_t) h * (int64_t) 4);
 
-	for ( int64_t i = x, I = 0; I < w; i++, I++ ) {
+  int64_t pixelbytes = 3 * sizeof(unsigned short);
+  int64_t chunkbytes = vmu_file->chunksize * pixelbytes;
+  unsigned short *buffer = (unsigned short *) g_slice_alloc(chunkbytes);
 
-	    int64_t cto = chunk_table[j][i / vmu_file->chunksize];
+  int64_t ct = -1;
+  for (int64_t j = y, J = 0; J < h; j++, J++) {
 
-	    if ( cto != ct ) {
+    for (int64_t i = x, I = 0; I < w; i++, I++) {
 
-		fseek( f, cto, SEEK_SET );
-		fread( buffer, chunkbytes, 1, f );
-		ct = cto;
+      int64_t cto = chunk_table[j][i / vmu_file->chunksize];
 
-	    }
-	    int64_t image_offset = ( ( J * w ) + I ) * 4;
-	    
-	    if ( ( 0 <= i ) && ( i < vmu_file->w ) &&
-		 ( 0 <= j ) && ( j < vmu_file->h ) ) {
-		
-		int64_t loc = ( i % vmu_file->chunksize ) * 3;
-		
-		imagedata[image_offset + 0] = *( buffer + loc + 2 ) >> 4;
-		imagedata[image_offset + 1] = *( buffer + loc + 1 ) >> 4;
-		imagedata[image_offset + 2] = *( buffer + loc + 0 ) >> 4;
-		imagedata[image_offset + 3] = 0xff;
-		
-	    }
-    
-	}
+      if (cto != ct) {
+
+	fseek(f, cto, SEEK_SET);
+	fread(buffer, chunkbytes, 1, f);
+	ct = cto;
+
+      }
+      int64_t image_offset = ((J * w) + I) * 4;
+
+      if ((0 <= i) && (i < vmu_file->w) && (0 <= j) && (j < vmu_file->h)) {
+
+	int64_t loc = (i % vmu_file->chunksize) * 3;
+
+	imagedata[image_offset + 0] = *(buffer + loc + 2) >> 4;
+	imagedata[image_offset + 1] = *(buffer + loc + 1) >> 4;
+	imagedata[image_offset + 2] = *(buffer + loc + 0) >> 4;
+	imagedata[image_offset + 3] = 0xff;
+
+      }
 
     }
 
-    fclose( f );
+  }
 
-    cairo_surface_t *surface = 
-	cairo_image_surface_create_for_data( ( unsigned char *) imagedata,
-					     CAIRO_FORMAT_ARGB32,
-					     w, h, w * 4 );
-    cairo_save(cr);
-    cairo_translate(cr, 0, 0 );
-    cairo_set_source_surface(cr, surface, 0, 0);
-    cairo_surface_destroy(surface);
-    cairo_paint(cr);
-    
-    cairo_restore(cr);
+  fclose(f);
 
-    g_slice_free( unsigned char *, imagedata );
-    g_slice_free( unsigned short *, buffer );
+  cairo_surface_t *surface =
+    cairo_image_surface_create_for_data((unsigned char *) imagedata,
+					CAIRO_FORMAT_ARGB32,
+					w, h, w * 4);
+  cairo_save(cr);
+  cairo_translate(cr, 0, 0);
+  cairo_set_source_surface(cr, surface, 0, 0);
+  cairo_surface_destroy(surface);
+  cairo_paint(cr);
+
+  cairo_restore(cr);
+
+  g_slice_free(unsigned char *, imagedata);
+  g_slice_free(unsigned short *, buffer);
 
 }
 
-static void paint_region(openslide_t *osr, cairo_t *cr,
+static void paint_region(openslide_t * osr, cairo_t * cr,
 			 int64_t x, int64_t y,
-			 int32_t layer,
-			 int32_t w, int32_t h) {
-    struct _openslide_vmuopsdata *data = osr->data;
-    
-    g_mutex_lock(data->vmu_mutex);
-    paint_region_unlocked(osr, cr, x, y, layer, w, h);
-    g_mutex_unlock(data->vmu_mutex);
+			 int32_t layer, int32_t w, int32_t h) {
+  struct _openslide_vmuopsdata *data = osr->data;
+
+  g_mutex_lock(data->vmu_mutex);
+  paint_region_unlocked(osr, cr, x, y, layer, w, h);
+  g_mutex_unlock(data->vmu_mutex);
 }
 
 
 static const struct _openslide_ops _openslide_vmu_ops = {
-    .get_dimensions = get_dimensions,
-    .paint_region = paint_region,
-    .destroy = destroy
+  .get_dimensions = get_dimensions,
+  .paint_region = paint_region,
+  .destroy = destroy
 };
 
-void _openslide_add_vmu_ops( openslide_t *osr,
-			     struct _openslide_hash *quickhash1,
-			     int32_t file_count,
-			     struct _openslide_vmu_file **files )
-
+void _openslide_add_vmu_ops(openslide_t * osr,
+			    struct _openslide_hash *quickhash1,
+			    int32_t file_count,
+			    struct _openslide_vmu_file **files)
 {
 
-    if (osr == NULL) {
-	return;
-    }
-    
-    // allocate private data
-    struct _openslide_vmuopsdata *data =
-	g_slice_new(struct _openslide_vmuopsdata);
-    
-    data->file_count = file_count;
-    data->files = files;
+  if (osr == NULL) {
+    return;
+  }
+  // allocate private data
+  struct _openslide_vmuopsdata *data =
+    g_slice_new(struct _openslide_vmuopsdata);
 
-    // populate private data
-    data->vmu_mutex = g_mutex_new();
-    
-    // generate hash of the smallest layer
-    _openslide_hash_file( quickhash1, files[0]->filename );
-    
-    // store vmu-specific data into osr
-    g_assert(osr->data == NULL);
-    
-    // general osr data
-    osr->layer_count = file_count;
-    osr->data = data;
-    osr->ops = &_openslide_vmu_ops;
+  data->file_count = file_count;
+  data->files = files;
+
+  // populate private data
+  data->vmu_mutex = g_mutex_new();
+
+  // generate hash of the smallest layer
+  _openslide_hash_file(quickhash1, files[0]->filename);
+
+  // store vmu-specific data into osr
+  g_assert(osr->data == NULL);
+
+  // general osr data
+  osr->layer_count = file_count;
+  osr->data = data;
+  osr->ops = &_openslide_vmu_ops;
 }
--- src/openslide-vendor-hamamatsu-vmu.c
+++ src/openslide-vendor-hamamatsu-vmu.c
@@ -1,7 +1,7 @@
 /*
  *  OpenSlide, a library for reading whole slide image files
  *
- *  Copyright ( c ) 2007-2010 Carnegie Mellon University
+ *  Copyright (c) 2007-2010 Carnegie Mellon University
  *  All rights reserved.
  *
  *  OpenSlide is free software: you can redistribute it and/or modify
@@ -20,7 +20,7 @@
  */
 
 /*
- * Hamamatsu ( VMU ) support
+ * Hamamatsu (VMU) support
  *
  * Steve Lamont <spl@ncmir.ucsd.edu>
  * National Center for Microscopy and Imaging Research
@@ -51,290 +51,273 @@ static const char KEY_IMAGE_FILE[] = "ImageFile";
 static const char KEY_NUM_LAYERS[] = "NoLayers";
 static const char KEY_MACRO_IMAGE[] = "MacroImage";
 
-static void add_properties( GHashTable *ht, GKeyFile *kf ) {
-    g_hash_table_insert( ht,
-			g_strdup( OPENSLIDE_PROPERTY_NAME_VENDOR ),
-			g_strdup( "hamamatsu" ) );
-    
-    char **keys = g_key_file_get_keys( kf, GROUP_VMU, NULL, NULL );
-    if ( keys == NULL ) {
-	return;
+static void add_properties(GHashTable *ht, GKeyFile *kf) {
+  g_hash_table_insert(ht,
+		      g_strdup(OPENSLIDE_PROPERTY_NAME_VENDOR),
+		      g_strdup("hamamatsu"));
+
+  char **keys = g_key_file_get_keys(kf, GROUP_VMU, NULL, NULL);
+  if (keys == NULL) {
+    return;
+  }
+
+  for (char **key = keys; *key != NULL; key++) {
+    char *value = g_key_file_get_value(kf, GROUP_VMU, *key, NULL);
+    if (value) {
+      g_hash_table_insert(ht,
+			  g_strdup_printf("hamamatsu.%s", *key),
+			  g_strdup(value));
+      g_free(value);
     }
-    
-    for ( char **key = keys; *key != NULL; key++ ) {
-	char *value = g_key_file_get_value( kf, GROUP_VMU, *key, NULL );
-	if ( value ) {
-	    g_hash_table_insert( ht,
-				g_strdup_printf( "hamamatsu.%s", *key ),
-				g_strdup( value ) );
-	    g_free( value );
-	}
-    }
-    
-    g_strfreev( keys );
+  }
+
+  g_strfreev(keys);
 }
 
-static void add_macro_associated_image( GHashTable *ht,
-				       FILE *f ) {
-    _openslide_add_jpeg_associated_image( ht, "macro", f );
+static void add_macro_associated_image(GHashTable *ht, FILE *f) {
+  _openslide_add_jpeg_associated_image(ht, "macro", f);
 }
 
-bool _openslide_try_hamamatsu_vmu( openslide_t *osr, const char *filename,
-				   struct _openslide_hash *quickhash1 )
-
+bool _openslide_try_hamamatsu_vmu(openslide_t *osr, const char *filename,
+				  struct _openslide_hash *quickhash1)
 {
 
-    char *dirname = g_path_get_dirname( filename );
-    char **image_filenames = NULL;
-    struct _openslide_vmu_file **files = NULL;
-    
-    char **all_keys = NULL;
-    
-    bool success = false;
-    char *tmp;
-    
-    // first, see if it's a VMU file
-    GKeyFile *vmu_file = g_key_file_new();
-    if ( !g_key_file_load_from_file( vmu_file, filename, G_KEY_FILE_NONE, NULL ) ) {
-	//    g_debug( "Can't load VMU file" );
-	goto FAIL;
-    }
-    if ( !g_key_file_has_group( vmu_file, GROUP_VMU ) ) {
-	//    g_warning( "Can't find VMU group" );
-	goto FAIL;
-    }
-    
-    // hash in the VMU file
-    _openslide_hash_file( quickhash1, filename );
-    
-    // this format has cols*rows vmu files, plus the map
-    int num_files = 2;
-    image_filenames = g_new0( char *, num_files );
-    files = g_new0( struct _openslide_vmu_file *, num_files );
-    
+  char *dirname = g_path_get_dirname(filename);
+  char **image_filenames = NULL;
+  struct _openslide_vmu_file **files = NULL;
 
-    // add properties
-    if ( osr ) {
-	add_properties( osr->properties, vmu_file );
-    }
-    
-    
-    // extract MapFile
-    tmp = g_key_file_get_string( vmu_file,
-				GROUP_VMU,
-				KEY_MAP_FILE,
-				NULL );
-    if ( tmp ) {
-	char *map_filename = g_build_filename( dirname, tmp, NULL );
-	
-	image_filenames[num_files - 1] = map_filename;
-	struct _openslide_vmu_file *file =
-	    g_slice_new0( struct _openslide_vmu_file );
-	files[num_files - 1] = file;
-	
-	// hash in the map file
-	_openslide_hash_file( quickhash1, map_filename );
-	
-	g_free( tmp );
-    } else {
-	g_warning( "Can't read map file" );
-	goto FAIL;
-    }
-    
-    
-    // now the ImageFile
-    all_keys = g_key_file_get_keys( vmu_file, GROUP_VMU, NULL, NULL );
-    char **tmp2;
-    for ( tmp2 = all_keys; *tmp2 != NULL; tmp2++ ) {
-	char *key = *tmp2;
-	char *value = g_key_file_get_string( vmu_file, GROUP_VMU, key, NULL );
-	
-	//    g_debug( "%s", key );
-	
-	if ( strncmp( KEY_IMAGE_FILE, key, strlen( KEY_IMAGE_FILE ) ) == 0 ) {
+  char **all_keys = NULL;
 
-	    image_filenames[0] = g_build_filename( dirname, value, NULL );
-		
-	    files[0] = g_slice_new0( struct _openslide_vmu_file );
+  bool success = false;
+  char *tmp;
 
-	}
-	g_free( value );
-    }
-    
-    // check image filenames ( the others are sort of optional )
-    for ( int i = 0; i < num_files; i++ ) {
-	if ( !image_filenames[i] ) {
-	    g_warning( "Can't read image filename %d", i );
-	    goto FAIL;
-	}
-    }
-    
-    // open files
-    
-    for ( int i = 0; i < num_files; i++ ) {
+  // first, see if it's a VMU file
+  GKeyFile *vmu_file = g_key_file_new();
+  if (!g_key_file_load_from_file(vmu_file, filename, G_KEY_FILE_NONE, NULL)) {
+    //    g_debug( "Can't load VMU file" );
+    goto FAIL;
+  }
+  if (!g_key_file_has_group(vmu_file, GROUP_VMU)) {
+    //    g_warning( "Can't find VMU group" );
+    goto FAIL;
+  }
+  // hash in the VMU file
+  _openslide_hash_file(quickhash1, filename);
 
-	struct _openslide_vmu_file *vf = files[i];
-	
-	vf->filename = g_strdup( image_filenames[i] );
-	
-	FILE *f;
-	if ( ( f = fopen( vf->filename, "rb" ) ) == NULL ) {
-	    g_warning( "Can't open VMU image file %s", vf->filename );
-	    goto FAIL;
-	}
-	
-	
-	fseeko( f, 0, SEEK_END );
-	vf->end_in_file = ftello( f );
-	if ( vf->end_in_file == -1 ) {
-	    g_warning( "Can't read file size for VMU image file %s", 
-		      vf->filename );
-	    fclose( f );
-	    goto FAIL;
-	}
+  // this format has cols*rows vmu files, plus the map
+  int num_files = 2;
+  image_filenames = g_new0(char *, num_files);
+  files = g_new0(struct _openslide_vmu_file *, num_files);
 
-	bool flipped;
-	u_int16_t key;
-	fseeko( f, 0, SEEK_SET );
-	if ( !fread( &key, sizeof( key ), 1, f ) ) {
 
-	    g_warning( "Unexpected end of file reading header of %s.",
-		       vf->filename );
-	    fclose( f );
-	    goto FAIL;
+  // add properties
+  if (osr) {
+    add_properties(osr->properties, vmu_file);
+  }
 
-	} else if ( memcmp( ( void * ) &key, ( void * ) "NG", 2 ) == 0 )
-	    flipped = false;
-	else if ( memcmp( ( void * ) &key, ( void * ) "GN", 2 ) == 0 )
-	    flipped = true;
-	else {
+  // extract MapFile
+  tmp = g_key_file_get_string(vmu_file, GROUP_VMU, KEY_MAP_FILE, NULL);
+  if (tmp) {
+    char *map_filename = g_build_filename(dirname, tmp, NULL);
 
-	    g_warning( "%s does not seem to be an NGR file.", vf->filename );
-	    fclose( f );
-	    goto FAIL;
+    image_filenames[num_files - 1] = map_filename;
+    struct _openslide_vmu_file *file =
+      g_slice_new0(struct _openslide_vmu_file);
+    files[num_files - 1] = file;
 
-	}
-	
-	fseeko( f, 4, SEEK_SET );
-	if ( !fread( &vf->w, sizeof( vf->w ), 1, f ) ) {
+    // hash in the map file
+    _openslide_hash_file(quickhash1, map_filename);
 
-	    g_warning( "Unexpected end of file reading header of %s.",
-		       vf->filename );
-	    fclose( f );
-	    goto FAIL;
+    g_free(tmp);
+  } else {
+    g_warning("Can't read map file");
+    goto FAIL;
+  }
 
-	}
 
-	if ( !fread( &vf->h, sizeof( vf->h ), 1, f ) ) {
+  // now the ImageFile
+  all_keys = g_key_file_get_keys(vmu_file, GROUP_VMU, NULL, NULL);
+  char **tmp2;
+  for (tmp2 = all_keys; *tmp2 != NULL; tmp2++) {
+    char *key = *tmp2;
+    char *value = g_key_file_get_string(vmu_file, GROUP_VMU, key, NULL);
 
-	    g_warning( "Unexpected end of file reading header of %s.",
-		       vf->filename );
-	    fclose( f );
-	    goto FAIL;
+    //    g_debug( "%s", key );
 
-	}
+    if (strncmp(KEY_IMAGE_FILE, key, strlen(KEY_IMAGE_FILE)) == 0) {
 
-	if ( !fread( &vf->chunksize, sizeof( vf->chunksize ), 1, f ) ) {
+      image_filenames[0] = g_build_filename(dirname, value, NULL);
 
-	    g_warning( "Unexpected end of file reading header of %s.",
-		       vf->filename );
-	    fclose( f );
-	    goto FAIL;
-
-	}
-
-	int32_t header_size;
-	fseeko( f, 24, SEEK_SET );
-	if ( !fread( &header_size, sizeof( header_size ), 1, f ) ) {
-
-	    g_warning( "Unexpected end of file reading header of %s.",
-		       vf->filename );
-	    fclose( f );
-	    goto FAIL;
-
-	}
-
-	vf->start_in_file = header_size;
-
-	// file is done now
-	fclose( f );
-
-	int32_t wchunks = vf->w / vf->chunksize;
-
-	vf->chunk_table = 
-	    ( int64_t ** ) g_malloc( sizeof( int64_t ) * vf->h );
-	vf->chunk_table[0] = 
-	    ( int64_t * ) g_malloc( sizeof( int64_t ) * vf->h * wchunks );
-	for ( int32_t o = 1; o < vf->h; o++ )
-	    vf->chunk_table[o] = vf->chunk_table[o - 1] + wchunks;
-
-	int64_t I = 0;
-	int64_t J = 0;
-
-	for ( int64_t from = 0; from < vf->w * vf->h; from += vf->chunksize ) {
-	    
-	    vf->chunk_table[J][I / vf->chunksize] = 
-		( from * 3 * sizeof( unsigned short ) ) + vf->start_in_file;
-	    
-	    J++;
-	    if ( J == vf->h ) {
-		
-		J = 0;
-		I += vf->chunksize;
-		
-	    }
-	    
-	}
-	
-    }
-    
-    // add macro image if present
-    if ( osr ) {
-
-	tmp = g_key_file_get_string( vmu_file,
-				    GROUP_VMU,
-				    KEY_MACRO_IMAGE,
-				    NULL );
-	if ( tmp ) {
-
-	    char *macro_filename = g_build_filename( dirname, tmp, NULL );
-	    FILE *macro_f = fopen( macro_filename, "rb" );
-	    if ( macro_f ) {
-		add_macro_associated_image( osr->associated_images, macro_f );
-		fclose( macro_f );
-	    }
-	    g_free( macro_filename );
-	    g_free( tmp );
-
-	}
+      files[0] = g_slice_new0(struct _openslide_vmu_file);
 
     }
-    
-    _openslide_add_vmu_ops( osr, quickhash1, num_files, files );
-    success = true;
-    goto DONE;
-    
+    g_free(value);
+  }
+
+  // check image filenames ( the others are sort of optional )
+  for (int i = 0; i < num_files; i++) {
+    if (!image_filenames[i]) {
+      g_warning("Can't read image filename %d", i);
+      goto FAIL;
+    }
+  }
+
+  // open files
+
+  for (int i = 0; i < num_files; i++) {
+
+    struct _openslide_vmu_file *vf = files[i];
+
+    vf->filename = g_strdup(image_filenames[i]);
+
+    FILE *f;
+    if ((f = fopen(vf->filename, "rb")) == NULL) {
+      g_warning("Can't open VMU image file %s", vf->filename);
+      goto FAIL;
+    }
+
+
+    fseeko(f, 0, SEEK_END);
+    vf->end_in_file = ftello(f);
+    if (vf->end_in_file == -1) {
+      g_warning("Can't read file size for VMU image file %s", vf->filename);
+      fclose(f);
+      goto FAIL;
+    }
+
+    bool flipped;
+    u_int16_t key;
+    fseeko(f, 0, SEEK_SET);
+    if (!fread(&key, sizeof(key), 1, f)) {
+
+      g_warning("Unexpected end of file reading header of %s.", vf->filename);
+      fclose(f);
+      goto FAIL;
+
+    } else if (memcmp((void *) &key, (void *) "NG", 2) == 0)
+      flipped = false;
+    else if (memcmp((void *) &key, (void *) "GN", 2) == 0)
+      flipped = true;
+    else {
+
+      g_warning("%s does not seem to be an NGR file.", vf->filename);
+      fclose(f);
+      goto FAIL;
+
+    }
+
+    fseeko(f, 4, SEEK_SET);
+    if (!fread(&vf->w, sizeof(vf->w), 1, f)) {
+
+      g_warning("Unexpected end of file reading header of %s.", vf->filename);
+      fclose(f);
+      goto FAIL;
+
+    }
+
+    if (!fread(&vf->h, sizeof(vf->h), 1, f)) {
+
+      g_warning("Unexpected end of file reading header of %s.", vf->filename);
+      fclose(f);
+      goto FAIL;
+
+    }
+
+    if (!fread(&vf->chunksize, sizeof(vf->chunksize), 1, f)) {
+
+      g_warning("Unexpected end of file reading header of %s.", vf->filename);
+      fclose(f);
+      goto FAIL;
+
+    }
+
+    int32_t header_size;
+    fseeko(f, 24, SEEK_SET);
+    if (!fread(&header_size, sizeof(header_size), 1, f)) {
+
+      g_warning("Unexpected end of file reading header of %s.", vf->filename);
+      fclose(f);
+      goto FAIL;
+
+    }
+
+    vf->start_in_file = header_size;
+
+    // file is done now
+    fclose(f);
+
+    int32_t wchunks = vf->w / vf->chunksize;
+
+    vf->chunk_table = (int64_t **) g_malloc(sizeof(int64_t) * vf->h);
+    vf->chunk_table[0] =
+      (int64_t *) g_malloc(sizeof(int64_t) * vf->h * wchunks);
+    for (int32_t o = 1; o < vf->h; o++)
+      vf->chunk_table[o] = vf->chunk_table[o - 1] + wchunks;
+
+    int64_t I = 0;
+    int64_t J = 0;
+
+    for (int64_t from = 0; from < vf->w * vf->h; from += vf->chunksize) {
+
+      vf->chunk_table[J][I / vf->chunksize] =
+	(from * 3 * sizeof(unsigned short)) + vf->start_in_file;
+
+      J++;
+      if (J == vf->h) {
+
+	J = 0;
+	I += vf->chunksize;
+
+      }
+
+    }
+
+  }
+
+  // add macro image if present
+  if (osr) {
+
+    tmp = g_key_file_get_string(vmu_file, GROUP_VMU, KEY_MACRO_IMAGE, NULL);
+    if (tmp) {
+
+      char *macro_filename = g_build_filename(dirname, tmp, NULL);
+      FILE *macro_f = fopen(macro_filename, "rb");
+      if (macro_f) {
+	add_macro_associated_image(osr->associated_images, macro_f);
+	fclose(macro_f);
+      }
+      g_free(macro_filename);
+      g_free(tmp);
+
+    }
+
+  }
+
+  _openslide_add_vmu_ops(osr, quickhash1, num_files, files);
+  success = true;
+  goto DONE;
+
 FAIL:
-    if ( files ) {
-	for ( int i = 0; i < num_files; i++ ) {
-	    g_free( files[i]->filename );
-	}
-	g_free( files );
+  if (files) {
+    for (int i = 0; i < num_files; i++) {
+      g_free(files[i]->filename);
     }
-    
-    success = false;
-    
+    g_free(files);
+  }
+
+  success = false;
+
 DONE:
-    g_strfreev( all_keys );
-    g_free( dirname );
-    
-    if ( image_filenames ) {
-	for ( int i = 0; i < num_files; i++ ) {
-	    g_free( image_filenames[i] );
-	}
-	g_free( image_filenames );
+  g_strfreev(all_keys);
+  g_free(dirname);
+
+  if (image_filenames) {
+    for (int i = 0; i < num_files; i++) {
+      g_free(image_filenames[i]);
     }
-    g_key_file_free( vmu_file );
-    
-    return success;
+    g_free(image_filenames);
+  }
+  g_key_file_free(vmu_file);
+
+  return success;
 }
