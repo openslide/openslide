309791c09ab61a9b786201230d6d41630785b454
Adam Goode <agoode@andrew.cmu.edu>
2010-05-06 12:10:03 -0400

    Reinstate the aperio warning_callback

--- src/openslide-vendor-aperio.c
+++ src/openslide-vendor-aperio.c
@@ -138,8 +138,8 @@ static void aperio_tiff_tilereader(TIFF *tiff,
     /* don't use info_handler, it outputs lots of junk */
     opj_event_mgr_t event_callbacks;
     event_callbacks.error_handler = error_callback;
-    event_callbacks.warning_handler = 0;
-    event_callbacks.info_handler = 0;
+    event_callbacks.warning_handler = warning_callback;
+    event_callbacks.info_handler = NULL;
 
     opj_set_event_mgr((opj_common_ptr) dinfo, &event_callbacks, NULL);
 
