0f512e05e4aacf3b8082936143b354bdc9204d0f
Adam Goode <agoode@andrew.cmu.edu>
2010-05-12 11:19:46 -0400

    Remove inttypes stuff from config.h.cmake

--- config.h.cmake
+++ config.h.cmake
@@ -1,13 +1,4 @@
 #ifndef OPENSLIDE_CONFIG_H
 #define OPENSLIDE_CONFIG_H
 
-/* defined if "inttypes.h" is availabel */
-#cmakedefine HAVE_INTTYPES_H
-
-/* 16 bit format */
-#cmakedefine PRIu16 @PRIu16@
-
-/* 64 bit format */
-#cmakedefine PRId64 @PRId64@
-
 #endif
