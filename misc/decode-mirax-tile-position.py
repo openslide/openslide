ddac21766ab7c50463514d92d94d507c6a77c1e2
Adam Goode <agoode@andrew.cmu.edu>
2010-07-30 13:48:38 -0400

    Look at all args in mirax decode tile

--- misc/decode-mirax-tile-position.py
+++ misc/decode-mirax-tile-position.py
@@ -2,29 +2,40 @@
 
 import struct, sys, os
 
-f = open(sys.argv[1], "rb")
-
 HEADER_SIZE = 296
 STRUCT_SIZE = 9
 
-f.seek(0, 2)
-filesize = f.tell()
+def is_correct_size(filename):
+    filesize = os.path.getsize(filename)
+    return (filesize - HEADER_SIZE) % STRUCT_SIZE == 0
 
-if (filesize - HEADER_SIZE) % STRUCT_SIZE != 0:
-    raise IOError("file not of expected size")
 
-f.seek(HEADER_SIZE)
+def try_reading(f):
+    f.seek(HEADER_SIZE)
 
-while True:
-    data = f.read(STRUCT_SIZE)
-    if len(data) == 0:
-        break
+    while True:
+        data = f.read(STRUCT_SIZE)
+        if len(data) == 0:
+            break
 
-    unpacked = struct.unpack("<iiB", data)
+        unpacked = struct.unpack("<iiB", data)
 
-    x = int(unpacked[0]) / 256.0
-    y = int(unpacked[1]) / 256.0
-    zz = int(unpacked[2])
+        x = int(unpacked[0]) / 256.0
+        y = int(unpacked[1]) / 256.0
+        zz = int(unpacked[2])
 
-    print '%25.8f %25.8f %10d' % (x, y, zz)
+        print '%25.8f %25.8f %10d' % (x, y, zz)
 
+
+
+
+for filename in sys.argv[1:]:
+    if not is_correct_size(filename):
+        print "%s: file not of expected size" % (filename)
+    else:
+        print filename
+        f = open(filename, "rb")
+        try:
+            try_reading(f)
+        finally:
+            f.close()
