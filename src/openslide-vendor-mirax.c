f1f0bdc3e4b66792ce254eb7c252b955147796dd
Adam Goode <agoode@andrew.cmu.edu>
2010-09-07 20:46:57 -0400

    Use read_le_int32_from_file_with_result in read_slide_position_file in mirax

--- src/openslide-vendor-mirax.c
+++ src/openslide-vendor-mirax.c
@@ -572,13 +572,14 @@ static int32_t *read_slide_position_file(const char *dirname, const char *name,
 
   for (int i = 0; i < count; i++) {
     // read 2 numbers, then a null
-    int32_t x = read_le_int32_from_file(f);
-    int32_t y = read_le_int32_from_file(f);
+    int32_t x;
+    int32_t y;
+    bool x_ok = read_le_int32_from_file_with_result(f, &x);
+    bool y_ok = read_le_int32_from_file_with_result(f, &y);
 
-    uint8_t zz;
-    int fread_result = fread(&zz, 1, 1, f);
+    int zz = getc(f);
 
-    if ((x == -1) || (y == -1) || ((zz != 0) && (zz != 255)) || (fread_result != 1)) {
+    if (!x_ok || !y_ok || (zz == EOF) || ((zz != 0) && (zz != 255))) {
       g_warning("Error while reading slide position file");
       fclose(f);
       g_free(result);
