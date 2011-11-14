a670a56ef175359b0a32ccb991686a8798a12b70
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2011-11-14 17:09:23 -0500

    Update CMake for FD_CLOEXEC changes

--- config.h.cmake
+++ config.h.cmake
@@ -4,4 +4,11 @@
 /* defined if "stdbool.h" is available */
 #cmakedefine HAVE_STDBOOL_H
 
+/* defined to the fopen() flag string that sets FD_CLOEXEC, or an empty
+   string if not supported. */
+#cmakedefine FOPEN_CLOEXEC_FLAG @FOPEN_CLOEXEC_FLAG@
+
+/* defined if fcntl() exists */
+#cmakedefine HAVE_FCNTL
+
 #endif
