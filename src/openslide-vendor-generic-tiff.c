a6fa2b25812490f1ca2d280e88599475c2d610dd
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2012-02-22 18:38:08 -0500

    generic-tiff: Fix directory skew when a directory is ignored
    When mapping slide layers to TIFF directories, the generic TIFF driver
    was not accounting for directories it had chosen to ignore.  For TIFFs
    containing unrelated directories between valid slide layers, this could
    cause OpenSlide to return incorrect image data or to refuse to open the
    slide.

--- src/openslide-vendor-generic-tiff.c
+++ src/openslide-vendor-generic-tiff.c
@@ -1,7 +1,7 @@
 /*
  *  OpenSlide, a library for reading whole slide image files
  *
- *  Copyright (c) 2007-2010 Carnegie Mellon University
+ *  Copyright (c) 2007-2012 Carnegie Mellon University
  *  Copyright (c) 2011 Google, Inc.
  *  All rights reserved.
  *
@@ -38,7 +38,7 @@
 #include <errno.h>
 
 struct layer {
-  int32_t layer_number;
+  int32_t directory;
   int64_t width;
 };
 
@@ -60,7 +60,6 @@ bool _openslide_try_generic_tiff(openslide_t *osr, TIFF *tiff,
   GList *layer_list = NULL;
   int32_t layer_count = 0;
   int32_t *layers = NULL;
-  int32_t current_layer = 0;
 
   if (!TIFFIsTiled(tiff)) {
     goto FAIL; // not tiled
@@ -73,48 +72,49 @@ bool _openslide_try_generic_tiff(openslide_t *osr, TIFF *tiff,
   }
 
   // accumulate tiled layers
-  current_layer = 0;
   layer_count = 0;
   do {
-    if (TIFFIsTiled(tiff)) {
-      // get width
-      uint32_t width;
-      if (!TIFFGetField(tiff, TIFFTAG_IMAGEWIDTH, &width)) {
-	// oh no
-	continue;
-      }
-
-      // confirm it is either the first image, or reduced-resolution
-      if (current_layer != 0) {
-	uint32_t subfiletype;
-	if (!TIFFGetField(tiff, TIFFTAG_SUBFILETYPE, &subfiletype)) {
-	  continue;
-	}
-
-	if (!(subfiletype & FILETYPE_REDUCEDIMAGE)) {
-	  continue;
-	}
-      }
-
-      // verify that we can read this compression (hard fail if not)
-      uint16_t compression;
-      if (!TIFFGetField(tiff, TIFFTAG_COMPRESSION, &compression)) {
-	g_warning("Can't read compression scheme");
-	goto FAIL;
-      };
-      if (!TIFFIsCODECConfigured(compression)) {
-	g_warning("Unsupported TIFF compression: %u", compression);
-	goto FAIL;
-      }
-
-      // push into list
-      struct layer *l = g_slice_new(struct layer);
-      l->layer_number = current_layer;
-      l->width = width;
-      layer_list = g_list_prepend(layer_list, l);
-      layer_count++;
+    // confirm that this directory is tiled
+    if (!TIFFIsTiled(tiff)) {
+      continue;
     }
-    current_layer++;
+
+    // get width
+    uint32_t width;
+    if (!TIFFGetField(tiff, TIFFTAG_IMAGEWIDTH, &width)) {
+      // oh no
+      continue;
+    }
+
+    // confirm it is either the first image, or reduced-resolution
+    if (TIFFCurrentDirectory(tiff) != 0) {
+      uint32_t subfiletype;
+      if (!TIFFGetField(tiff, TIFFTAG_SUBFILETYPE, &subfiletype)) {
+        continue;
+      }
+
+      if (!(subfiletype & FILETYPE_REDUCEDIMAGE)) {
+        continue;
+      }
+    }
+
+    // verify that we can read this compression (hard fail if not)
+    uint16_t compression;
+    if (!TIFFGetField(tiff, TIFFTAG_COMPRESSION, &compression)) {
+      g_warning("Can't read compression scheme");
+      goto FAIL;
+    };
+    if (!TIFFIsCODECConfigured(compression)) {
+      g_warning("Unsupported TIFF compression: %u", compression);
+      goto FAIL;
+    }
+
+    // push into list
+    struct layer *l = g_slice_new(struct layer);
+    l->directory = TIFFCurrentDirectory(tiff);
+    l->width = width;
+    layer_list = g_list_prepend(layer_list, l);
+    layer_count++;
   } while (TIFFReadDirectory(tiff));
 
   // sort tiled layers
@@ -126,7 +126,7 @@ bool _openslide_try_generic_tiff(openslide_t *osr, TIFF *tiff,
     struct layer *l = (struct layer *)layer_list->data;
     layer_list = g_list_delete_link(layer_list, layer_list);
 
-    layers[i] = l->layer_number;
+    layers[i] = l->directory;
     g_slice_free(struct layer, l);
   }
 
