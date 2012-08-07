025614fc0caec70538a5f6f2d55204e265f1c1c9
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2012-08-07 17:09:26 -0400

    Add extern "C" declarations to public headers
    so C++ programs don't need to wrap the #include themselves.  This seems
    to be common practice, so follow the principle of least surprise.

--- src/openslide.h
+++ src/openslide.h
@@ -33,6 +33,10 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
  * The main OpenSlide type.
  */
@@ -471,5 +475,9 @@ void _openslide_cancel_prefetch_hint_UNIMPLEMENTED(void);
  * whole-slide images (also known as virtual slides). See
  * http://openslide.org/ for more details.
  */
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
