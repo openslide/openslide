23739264086dd4551e02d26dfb09dd25fadcf0cb
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2012-08-11 01:23:49 -0400

    Add bulk testcase helper

---
+++ misc/bulk-testcase-helper.sh
@@ -0,0 +1,33 @@
+# Shell functions for mass-producing test cases.
+# Variables:
+#     slide: the base slide (e.g. Mirax/CMU-1.zip)
+#     prefix: an optional prefix for test case names (e.g. mirax)
+#     EDITOR: an optional text editor to use
+
+function start() {
+    # Argument: basename of new test case
+    # Must be run from /test directory.
+    if [ -n "$prefix" ] ; then
+        testcase="$prefix-$1"
+    else
+        testcase="$1"
+    fi
+
+    ./driver create "$testcase" "$slide"
+    pushd "cases/$testcase"
+    [ -n "$EDITOR" ] && $EDITOR config.yaml
+}
+
+function finish() {
+    if [ -n "$testcase" ] ; then
+        popd
+        ./driver pack $testcase
+    fi
+    testcase=
+}
+
+function next() {
+    # Argument: basename of new test case
+    finish
+    start "$1"
+}
