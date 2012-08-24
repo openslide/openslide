37745893eecf008650dcb96fd8191c9cebbcf47a
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2012-08-24 01:41:54 -0400

    aperio: Don't report OpenJPEG warnings
    We can either ignore warnings or treat them as fatal.  OpenJPEG appears
    to have some warning-class messages that should not be fatal, so we opt
    to ignore.

--- src/openslide-vendor-aperio.c
+++ src/openslide-vendor-aperio.c
@@ -76,8 +76,9 @@ static void write_pixel_rgb(uint32_t *dest, uint8_t c0, uint8_t c1, uint8_t c2) 
   *dest = 255 << 24 | c0 << 16 | c1 << 8 | c2;
 }
 
-static void warning_callback(const char *msg, void *data G_GNUC_UNUSED) {
-  g_warning("%s", msg);
+static void warning_callback(const char *msg G_GNUC_UNUSED,
+                             void *data G_GNUC_UNUSED) {
+  //g_debug("%s", msg);
 }
 static void error_callback(const char *msg, void *data) {
   openslide_t *osr = data;
