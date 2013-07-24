725039e688cddeaa3b97f007ee0682ea1b08f147
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2013-07-24 00:10:12 -0400

    Update package-wide copyright dates for 2013

--- src/openslide.c
+++ src/openslide.c
@@ -33,7 +33,7 @@
 #include "openslide-tilehelper.h"
 #include "openslide-cairo.h"
 
-const char _openslide_release_info[] = "OpenSlide " PACKAGE_VERSION ", copyright (C) 2007-2012 Carnegie Mellon University and others.\nLicensed under the GNU Lesser General Public License, version 2.1.";
+const char _openslide_release_info[] = "OpenSlide " PACKAGE_VERSION ", copyright (C) 2007-2013 Carnegie Mellon University and others.\nLicensed under the GNU Lesser General Public License, version 2.1.";
 
 static const char * const EMPTY_STRING_ARRAY[] = { NULL };
 
