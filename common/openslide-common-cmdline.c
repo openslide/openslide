e01825b4959ad92f56e223f277f0b3b2a1df133c
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2016-06-12 15:49:40 -0500

    Update package-wide copyrights for 2016

--- common/openslide-common-cmdline.c
+++ common/openslide-common-cmdline.c
@@ -35,7 +35,7 @@
 
 static const char *version_format = "%s " SUFFIXED_VERSION ", "
 "using OpenSlide %s\n"
-"Copyright (C) 2007-2015 Carnegie Mellon University and others\n"
+"Copyright (C) 2007-2016 Carnegie Mellon University and others\n"
 "\n"
 "OpenSlide is free software: you can redistribute it and/or modify it under\n"
 "the terms of the GNU Lesser General Public License, version 2.1.\n"
