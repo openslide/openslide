d353c70b2f202479a221dc1ec5bb8876d758523b
Luke Novak <luke.novak@pathai.com>
2021-11-04 18:06:12 +0000

    Fix BIF slide width errors in 16px and AOI alignment
    Signed-off-by: Luke Novak <luke.novak@pathai.com>

--- src/openslide-vendor-ventana.c
+++ src/openslide-vendor-ventana.c
@@ -730,6 +730,16 @@ static struct _openslide_grid *create_bif_grid(openslide_t *osr,
                                    bif->tile_advance_y / downsample,
                                    read_subtile_tilemap, NULL);
 
+  // get start x and y coordinates of the slide as a whole
+  double slide_start_x = INFINITY;
+  double slide_start_y = INFINITY;
+  for (int32_t i = 0; i < bif->num_areas; i++) {
+    double area_start_x = bif->areas[i]->x;
+    double area_start_y = bif->areas[i]->y;
+    slide_start_x = (area_start_x < slide_start_x) ? area_start_x : slide_start_x;
+    slide_start_y = (area_start_y < slide_start_y) ? area_start_y : slide_start_y;
+  }
+
   for (int32_t i = 0; i < bif->num_areas; i++) {
     struct area *area = bif->areas[i];
     //g_debug("ds %g area %d pos %"PRId64" %"PRId64" offset %g %g", downsample, i, area->x, area->y, offset_x, offset_y);
@@ -738,16 +748,21 @@ static struct _openslide_grid *create_bif_grid(openslide_t *osr,
     // offset value. Here we accumulate the total offsets in each direction as
     // we iterate through the tiles to prevent gaps from appearing.
 
+    // Each area has an x and y offset to ensure that it starts at area->x in the tilemap
+    // grid. Without this offset, it will start at the start row/column times the tile width
+    // which is not always correct due to accumulated overlaps/offsets in previous areas
+    double area_offset_x = area->x - (area->start_col * bif->tile_advance_x + slide_start_x);
+    double area_offset_y = area->y - (area->start_row * bif->tile_advance_y + slide_start_y);
+
     // cumulative offset_y for each column
     double offset_ys[area->tiles_across];
     for (int64_t col = 0; col < area->tiles_across; col++) {
-      // preserve pre-DP 200 behavior
-      offset_ys[col] = is_dp200 ? 0 : area->y - area->start_row * bif->tile_advance_y;
+      offset_ys[col] = area_offset_y;
     }
 
     for (int64_t row = 0; row < area->tiles_down; row++) {
-      // cumulative offset_x for this row; preserve pre-DP 200 behavior
-      double offset_x = is_dp200 ? 0 : area->x - area->start_col * bif->tile_advance_x;
+      // cumulative offset_x for this row;
+      double offset_x = area_offset_x;
       for (int64_t col = 0; col < area->tiles_across; col++) {
         if (is_dp200) {
           // use the tile offsets only for DP 200 scans.
@@ -929,8 +944,9 @@ static bool ventana_open(openslide_t *osr, const char *filename,
         // large enough for all the pixels
         double x, y, w, h;
         _openslide_grid_get_bounds(l->grid, &x, &y, &w, &h);
-        l->base.w = ceil(x + w);
-        l->base.h = ceil(y + h);
+        // adjust height and width so it is 16 px aligned on dp200 slides   
+        l->base.w = is_dp200 ? ceil(ceil(x + w) / 16) * 16 : ceil(x + w);
+        l->base.h = is_dp200 ? ceil(ceil(y + h) / 16) * 16 : ceil(y + h);
         // clear tile size hints set by _openslide_tiff_level_init()
         l->base.tile_w = 0;
         l->base.tile_h = 0;
