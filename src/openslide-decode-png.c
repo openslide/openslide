826cb609be8a1e93028e4a1e4e85bce182d14e1d
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2022-08-05 20:56:03 -0400

    png: convert png_read to g_autoptr
    Use __attribute__((noinline)) to avoid inlining that causes gcc 12.1.1
    to emit a setjmp clobber warning.  This isn't ideal, but the glib wrapper
    isn't available in glib 2.56, and we're already dependent on GNU C for
    the cleanup attribute.
    
    Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

--- src/openslide-decode-png.c
+++ src/openslide-decode-png.c
@@ -3,6 +3,7 @@
  *
  *  Copyright (c) 2007-2013 Carnegie Mellon University
  *  Copyright (c) 2011 Google, Inc.
+ *  Copyright (c) 2022 Benjamin Gilbert
  *  All rights reserved.
  *
  *  OpenSlide is free software: you can redistribute it and/or modify
@@ -32,7 +33,11 @@
 #include <setjmp.h>
 #include <stdio.h>
 
-struct png_error_ctx {
+struct png_ctx {
+  png_struct *png;
+  png_info *info;
+  png_byte **rows;
+  int64_t h;
   jmp_buf env;
   GError *err;
 };
@@ -43,124 +48,140 @@ static void warning_callback(png_struct *png G_GNUC_UNUSED,
 }
 
 static void error_callback(png_struct *png, const char *message) {
-  struct png_error_ctx *ectx = png_get_error_ptr(png);
-  g_set_error(&ectx->err, OPENSLIDE_ERROR, OPENSLIDE_ERROR_FAILED,
+  struct png_ctx *ctx = png_get_error_ptr(png);
+  g_set_error(&ctx->err, OPENSLIDE_ERROR, OPENSLIDE_ERROR_FAILED,
               "PNG error: %s", message);
-  longjmp(ectx->env, 1);
+  longjmp(ctx->env, 1);
+}
+
+static void png_ctx_free(struct png_ctx *ctx) {
+  png_destroy_read_struct(&ctx->png, &ctx->info, NULL);
+  g_slice_free1(ctx->h * sizeof(*ctx->rows), ctx->rows);
+  g_slice_free(struct png_ctx, ctx);
+}
+
+typedef struct png_ctx png_ctx;
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(png_ctx, png_ctx_free)
+
+// We want to avoid inlining this to avoid compiler setjmp clobber warnings
+// in the caller.  We won't have G_GNUC_NO_INLINE until glib 2.58, so use
+// the raw attribute syntax for now.
+static struct png_ctx * __attribute__((noinline)) png_ctx_new(uint32_t *dest,
+                                                              int64_t w,
+                                                              int64_t h,
+                                                              GError **err) {
+  g_autoptr(png_ctx) ctx = g_slice_new0(struct png_ctx);
+
+  // allocate row pointers
+  ctx->rows = g_slice_alloc(h * sizeof(*ctx->rows));
+  ctx->h = h;
+  for (int64_t y = 0; y < h; y++) {
+    ctx->rows[y] = (png_byte *) &dest[y * w];
+  }
+
+  // init libpng
+  ctx->png = png_create_read_struct(PNG_LIBPNG_VER_STRING, ctx,
+                                    error_callback, warning_callback);
+  if (!ctx->png) {
+    g_set_error(err, OPENSLIDE_ERROR, OPENSLIDE_ERROR_FAILED,
+                "Couldn't initialize libpng");
+    return NULL;
+  }
+  ctx->info = png_create_info_struct(ctx->png);
+  if (!ctx->info) {
+    g_set_error(err, OPENSLIDE_ERROR, OPENSLIDE_ERROR_FAILED,
+                "Couldn't initialize PNG info");
+    return NULL;
+  }
+
+  return g_steal_pointer(&ctx);
 }
 
 static bool png_read(png_rw_ptr read_callback, void *callback_data,
                      uint32_t *dest, int64_t w, int64_t h,
                      GError **err) {
-  png_struct *png = NULL;
-  png_info *info = NULL;
-  volatile bool success = false;
-
-  // allocate error context
-  struct png_error_ctx *ectx = g_slice_new0(struct png_error_ctx);
-
-  // allocate row pointers
-  png_byte **rows = g_slice_alloc(h * sizeof(*rows));
-  for (int64_t y = 0; y < h; y++) {
-    rows[y] = (png_byte *) &dest[y * w];
+  // allocate context
+  g_autoptr(png_ctx) ctx = png_ctx_new(dest, w, h, err);
+  if (ctx == NULL) {
+    return false;
   }
 
-  // init libpng
-  png = png_create_read_struct(PNG_LIBPNG_VER_STRING, ectx,
-                               error_callback, warning_callback);
-  if (!png) {
-    g_set_error(err, OPENSLIDE_ERROR, OPENSLIDE_ERROR_FAILED,
-                "Couldn't initialize libpng");
-    goto DONE;
-  }
-  info = png_create_info_struct(png);
-  if (!info) {
-    g_set_error(err, OPENSLIDE_ERROR, OPENSLIDE_ERROR_FAILED,
-                "Couldn't initialize PNG info");
-    goto DONE;
-  }
-
-  if (!setjmp(ectx->env)) {
+  if (!setjmp(ctx->env)) {
     // We can't use png_init_io(): passing FILE * between libraries isn't
     // safe on Windows
-    png_set_read_fn(png, callback_data, read_callback);
+    png_set_read_fn(ctx->png, callback_data, read_callback);
 
     // read header
-    png_read_info(png, info);
-    int64_t width = png_get_image_width(png, info);
-    int64_t height = png_get_image_height(png, info);
+    png_read_info(ctx->png, ctx->info);
+    int64_t width = png_get_image_width(ctx->png, ctx->info);
+    int64_t height = png_get_image_height(ctx->png, ctx->info);
     if (width != w || height != h) {
       g_set_error(err, OPENSLIDE_ERROR, OPENSLIDE_ERROR_FAILED,
                   "Dimensional mismatch reading PNG: "
                   "expected %"PRId64"x%"PRId64", found %"PRId64"x%"PRId64,
                   w, h, width, height);
-      goto DONE;
+      return false;
     }
 
     // downsample 16 bits/channel to 8
     #ifdef PNG_READ_SCALE_16_TO_8_SUPPORTED
-      png_set_scale_16(png);
+      png_set_scale_16(ctx->png);
     #else
       // less-accurate fallback
-      png_set_strip_16(png);
+      png_set_strip_16(ctx->png);
     #endif
     // expand to 24-bit RGB or 8-bit gray
-    png_set_expand(png);
+    png_set_expand(ctx->png);
     // expand gray to 24-bit RGB
-    png_set_gray_to_rgb(png);
+    png_set_gray_to_rgb(ctx->png);
     // libpng emits bytes, but we need words, so byte order matters
     if (G_BYTE_ORDER == G_LITTLE_ENDIAN) {
       // need BGRA
       // RGB -> BGR, RGBA -> BGRA
-      png_set_bgr(png);
+      png_set_bgr(ctx->png);
       // BGR -> BGRx (BGR + filler)
-      png_set_filler(png, 0xff, PNG_FILLER_AFTER);
+      png_set_filler(ctx->png, 0xff, PNG_FILLER_AFTER);
     } else {
       // need ARGB
       // RGBA -> ARGB
-      png_set_swap_alpha(png);
+      png_set_swap_alpha(ctx->png);
       // RGB -> xRGB (filler + RGB)
-      png_set_filler(png, 0xff, PNG_FILLER_BEFORE);
+      png_set_filler(ctx->png, 0xff, PNG_FILLER_BEFORE);
     }
 
     // check buffer size
-    png_read_update_info(png, info);
-    uint32_t rowbytes = png_get_rowbytes(png, info);
+    png_read_update_info(ctx->png, ctx->info);
+    uint32_t rowbytes = png_get_rowbytes(ctx->png, ctx->info);
     if (rowbytes != w * sizeof(*dest)) {
       g_set_error(err, OPENSLIDE_ERROR, OPENSLIDE_ERROR_FAILED,
                   "Unexpected bufsize %u for %"PRId64" pixels",
                   rowbytes, w);
-      goto DONE;
+      return false;
     }
 
     // alpha channel is not supported
     // When adding support for PNGs with alpha, we will need to premultiply
     // the RGB channels.  libpng >= 1.5.4 supports premultiplied alpha via
     // png_set_alpha_mode().
-    int color_type = png_get_color_type(png, info);
+    int color_type = png_get_color_type(ctx->png, ctx->info);
     if (color_type != PNG_COLOR_TYPE_RGB) {
       g_set_error(err, OPENSLIDE_ERROR, OPENSLIDE_ERROR_FAILED,
                   "Unsupported color type %d", color_type);
-      goto DONE;
+      return false;
     }
 
     // read image
-    png_read_image(png, rows);
+    png_read_image(ctx->png, ctx->rows);
 
     // finish
-    png_read_end(png, NULL);
-
-    success = true;
+    png_read_end(ctx->png, NULL);
   } else {
     // setjmp returned again
-    g_propagate_error(err, ectx->err);
+    g_propagate_error(err, ctx->err);
+    return false;
   }
 
-DONE:
-  png_destroy_read_struct(&png, &info, NULL);
-  g_slice_free1(h * sizeof(*rows), rows);
-  g_slice_free(struct png_error_ctx, ectx);
-  return success;
+  return true;
 }
 
 static void file_read_callback(png_struct *png, png_byte *buf, png_size_t len) {
