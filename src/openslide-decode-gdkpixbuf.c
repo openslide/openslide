f73a04f58976bc399df8676b7ce82fd9ad8a5c8c
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2022-08-06 00:37:09 -0400

    gdkpixbuf: convert gdkpixbuf_read to g_autoptr
    The restructured error handling code is not exactly equivalent to the
    original, but shouldn't change the result in expected cases.
    
    Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

--- src/openslide-decode-gdkpixbuf.c
+++ src/openslide-decode-gdkpixbuf.c
@@ -3,6 +3,7 @@
  *
  *  Copyright (c) 2007-2014 Carnegie Mellon University
  *  Copyright (c) 2011 Google, Inc.
+ *  Copyright (c) 2022 Benjamin Gilbert
  *  All rights reserved.
  *
  *  OpenSlide is free software: you can redistribute it and/or modify
@@ -38,11 +39,12 @@
 
 #define BUFSIZE (64 << 10)
 
-struct load_state {
+struct gdkpixbuf_ctx {
+  GdkPixbufLoader *loader;
   int32_t w;
   int32_t h;
   GdkPixbuf *pixbuf;  // NULL until validated, then a borrowed ref
-  GError *err;
+  GError *err;        // from area_prepared
 };
 
 // Validate image size and format.  There's no point connecting to the
@@ -50,9 +52,9 @@ struct load_state {
 // after every BUFSIZE bytes, and we'll likely receive both signals while
 // processing the first buffer.
 static void area_prepared(GdkPixbufLoader *loader, void *data) {
-  struct load_state *state = data;
+  struct gdkpixbuf_ctx *ctx = data;
 
-  if (state->err) {
+  if (ctx->err) {
     return;
   }
 
@@ -65,76 +67,112 @@ static void area_prepared(GdkPixbufLoader *loader, void *data) {
       gdk_pixbuf_get_bits_per_sample(pixbuf) != 8 ||
       gdk_pixbuf_get_has_alpha(pixbuf) ||
       gdk_pixbuf_get_n_channels(pixbuf) != 3) {
-    g_set_error(&state->err, OPENSLIDE_ERROR, OPENSLIDE_ERROR_FAILED,
+    g_set_error(&ctx->err, OPENSLIDE_ERROR, OPENSLIDE_ERROR_FAILED,
                 "Unsupported pixbuf parameters");
     return;
   }
   int w = gdk_pixbuf_get_width(pixbuf);
   int h = gdk_pixbuf_get_height(pixbuf);
-  if (w != state->w || h != state->h) {
-    g_set_error(&state->err, OPENSLIDE_ERROR, OPENSLIDE_ERROR_FAILED,
+  if (w != ctx->w || h != ctx->h) {
+    g_set_error(&ctx->err, OPENSLIDE_ERROR, OPENSLIDE_ERROR_FAILED,
                 "Dimensional mismatch reading pixbuf: "
-                "expected %dx%d, found %dx%d", state->w, state->h, w, h);
+                "expected %dx%d, found %dx%d", ctx->w, ctx->h, w, h);
     return;
   }
 
   // commit
-  state->pixbuf = pixbuf;
+  ctx->pixbuf = pixbuf;
+}
+
+static void gdkpixbuf_ctx_free(struct gdkpixbuf_ctx *ctx) {
+  if (ctx->loader) {
+    gdk_pixbuf_loader_close(ctx->loader, NULL);
+    g_object_unref(ctx->loader);
+  }
+  g_clear_error(&ctx->err);
+  g_slice_free(struct gdkpixbuf_ctx, ctx);
+}
+
+typedef struct gdkpixbuf_ctx gdkpixbuf_ctx;
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(gdkpixbuf_ctx, gdkpixbuf_ctx_free)
+
+// Fix up an existing gdk-pixbuf error in *err, or possibly propagate a new
+// one.  Return false in the latter case.  Unlike the typical GError rules,
+// *err may be non-NULL.
+static bool gdkpixbuf_ctx_check_error(struct gdkpixbuf_ctx *ctx,
+                                      GError **err) {
+  g_prefix_error(err, "gdk-pixbuf error: ");
+  if (ctx->err) {
+    // error from area_prepared takes precedence
+    g_clear_error(err);
+    g_propagate_error(err, g_steal_pointer(&ctx->err));
+    return false;
+  }
+  return true;
+}
+
+static struct gdkpixbuf_ctx *gdkpixbuf_ctx_new(const char *format,
+                                               int32_t w, int32_t h,
+                                               GError **err) {
+  g_autoptr(gdkpixbuf_ctx) ctx = g_slice_new0(struct gdkpixbuf_ctx);
+  ctx->w = w;
+  ctx->h = h;
+
+  ctx->loader = gdk_pixbuf_loader_new_with_type(format, err);
+  if (!ctx->loader) {
+    return NULL;
+  }
+  g_signal_connect(ctx->loader,
+                   "area-prepared", G_CALLBACK(area_prepared), ctx);
+
+  return g_steal_pointer(&ctx);
 }
 
 static bool gdkpixbuf_read(const char *format,
                            size_t (*read_callback)(void *out, void *in, size_t size),
                            void *callback_data,
-                           int64_t length,
+                           uint64_t length,
                            uint32_t *dest,
                            int32_t w, int32_t h,
                            GError **err) {
-  GdkPixbufLoader *loader = NULL;
-  uint8_t *buf = g_slice_alloc(BUFSIZE);
-  bool success = false;
-  struct load_state state = {
-    .w = w,
-    .h = h,
-  };
-
   // create loader
-  loader = gdk_pixbuf_loader_new_with_type(format, err);
-  if (!loader) {
-    goto DONE;
+  g_autoptr(gdkpixbuf_ctx) ctx = gdkpixbuf_ctx_new(format, w, h, err);
+  if (!ctx) {
+    return false;
   }
-  g_signal_connect(loader, "area-prepared", G_CALLBACK(area_prepared), &state);
 
   // read data
+  g_auto(_openslide_slice) box = _openslide_slice_alloc(BUFSIZE);
   while (length) {
-    size_t count = read_callback(buf, callback_data, MIN(length, BUFSIZE));
+    size_t count = read_callback(box.p, callback_data, MIN(length, box.len));
     if (!count) {
       g_set_error(err, OPENSLIDE_ERROR, OPENSLIDE_ERROR_FAILED,
                   "Short read loading pixbuf");
-      goto DONE;
+      return false;
     }
-    if (!gdk_pixbuf_loader_write(loader, buf, count, err)) {
-      g_prefix_error(err, "gdk-pixbuf error: ");
-      goto DONE;
+    if (!gdk_pixbuf_loader_write(ctx->loader, box.p, count, err)) {
+      gdkpixbuf_ctx_check_error(ctx, err);
+      return false;
     }
-    if (state.err) {
-      goto DONE;
+    if (!gdkpixbuf_ctx_check_error(ctx, err)) {
+      return false;
     }
     length -= count;
   }
 
   // finish load
-  if (!gdk_pixbuf_loader_close(loader, err)) {
-    g_prefix_error(err, "gdk-pixbuf error: ");
-    goto DONE;
+  if (!gdk_pixbuf_loader_close(ctx->loader, err)) {
+    gdkpixbuf_ctx_check_error(ctx, err);
+    return false;
   }
-  if (state.err) {
-    goto DONE;
+  if (!gdkpixbuf_ctx_check_error(ctx, err)) {
+    return false;
   }
-  g_assert(state.pixbuf);
+  g_assert(ctx->pixbuf);
 
   // copy pixels
-  uint8_t *pixels = gdk_pixbuf_get_pixels(state.pixbuf);
-  int rowstride = gdk_pixbuf_get_rowstride(state.pixbuf);
+  uint8_t *pixels = gdk_pixbuf_get_pixels(ctx->pixbuf);
+  int rowstride = gdk_pixbuf_get_rowstride(ctx->pixbuf);
   for (int32_t y = 0; y < h; y++) {
     for (int32_t x = 0; x < w; x++) {
       dest[y * w + x] = 0xFF000000 |                              // A
@@ -144,26 +182,7 @@ static bool gdkpixbuf_read(const char *format,
     }
   }
 
-  success = true;
-
-DONE:
-  // clean up
-  if (loader) {
-    gdk_pixbuf_loader_close(loader, NULL);
-    g_object_unref(loader);
-  }
-  g_slice_free1(BUFSIZE, buf);
-
-  // now that the loader is closed, we know state.err won't be set
-  // behind our back
-  if (state.err) {
-    // signal handler validation errors override GdkPixbuf errors
-    g_clear_error(err);
-    g_propagate_error(err, state.err);
-    // signal handler errors should have been noticed before falling through
-    g_assert(!success);
-  }
-  return success;
+  return true;
 }
 
 static size_t file_read_callback(void *out, void *in, size_t size) {
