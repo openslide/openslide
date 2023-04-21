efd91185be87bd40cc56ffa74b0a3e5339407f87
Alexandr Virodov <alexandr.virodov@uky.edu>
2023-04-21 15:14:18 +0000

    Implementation of openslide-vendor-philips-isyntax.c using libisyntax C api.
    Signed-off-by: Alexandr Virodov <alexandr.virodov@uky.edu>

--- src/openslide-private.h
+++ src/openslide-private.h
@@ -133,6 +133,7 @@ extern const struct _openslide_format _openslide_format_hamamatsu_vms_vmu;
 extern const struct _openslide_format _openslide_format_leica;
 extern const struct _openslide_format _openslide_format_mirax;
 extern const struct _openslide_format _openslide_format_philips;
+extern const struct _openslide_format _openslide_format_philips_isyntax;
 extern const struct _openslide_format _openslide_format_sakura;
 extern const struct _openslide_format _openslide_format_synthetic;
 extern const struct _openslide_format _openslide_format_trestle;
