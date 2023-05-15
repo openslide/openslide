56593e7901f6a7c282e3cbc8e6b983e7921cac85
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2023-05-15 07:27:48 -0400

    Rename "philips" to "philips-tiff" internally
    There are plans to add a Philips iSyntax driver.  From the perspective of
    the openslide.vendor property these will both be "philips", but the two
    drivers will share no code.  Clarify that the existing driver is for the
    TIFF format.
    
    Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

--- src/openslide-private.h
+++ src/openslide-private.h
@@ -132,7 +132,7 @@ extern const struct _openslide_format _openslide_format_hamamatsu_ndpi;
 extern const struct _openslide_format _openslide_format_hamamatsu_vms_vmu;
 extern const struct _openslide_format _openslide_format_leica;
 extern const struct _openslide_format _openslide_format_mirax;
-extern const struct _openslide_format _openslide_format_philips;
+extern const struct _openslide_format _openslide_format_philips_tiff;
 extern const struct _openslide_format _openslide_format_sakura;
 extern const struct _openslide_format _openslide_format_synthetic;
 extern const struct _openslide_format _openslide_format_trestle;
