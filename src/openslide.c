openslide/openslide

commit f19d06e93a06f42e4d06ef6c0b82172736766498

Clear openslide_read_associated_image() output buffer on error
Match the semantics of openslide_read_region(), allowing us to avoid an
extra copy on success.  This is an ABI change.

Fixes https://github.com/openslide/openslide/issues/122.

Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

src/openslide.c
@@ -581,18 +581,20 @@ void openslide_read_associated_image(openslide_t *osr,
   struct _openslide_associated_image *img = g_hash_table_lookup(osr->associated_images,
 								name);
   if (img) {
-    // this function is documented to do nothing on failure, so we need an
-    // extra memcpy
     size_t pixels = img->w * img->h;
-    g_autofree uint32_t *buf = g_new(uint32_t, pixels);
+    g_autofree uint32_t *tmp_buf = NULL;
+    if (!dest) {
+      // undocumented special case for testing
+      tmp_buf = g_new(uint32_t, pixels);
+    }
 
     GError *tmp_err = NULL;
-    if (img->ops->get_argb_data(img, buf, &tmp_err)) {
-      if (dest) {
-        memcpy(dest, buf, pixels * sizeof(uint32_t));
-      }
-    } else {
+    if (!img->ops->get_argb_data(img, dest ? dest : tmp_buf, &tmp_err)) {
       _openslide_propagate_error(osr, tmp_err);
+      if (dest) {
+        // ensure we don't return a partial result
+        memset(dest, 0, pixels * sizeof(uint32_t));
+      }
     }
   }
 }

src/openslide.h
@@ -440,7 +440,10 @@ void openslide_get_associated_image_dimensions(openslide_t *osr,
  * with a whole slide image. @p dest must be a valid pointer to enough
  * memory to hold the image, at least (width * height * 4) bytes in
  * length.  Get the width and height with
- * openslide_get_associated_image_dimensions(). This function does nothing
+ * openslide_get_associated_image_dimensions().
+ *
+ * If an error occurs or has occurred, then the memory pointed to by @p dest
+ * will be cleared. In versions prior to 4.0.0, this function did nothing
  * if an error occurred.
  *
  * For more information about processing pre-multiplied pixel data, see

commit 73d6a3cc654cc1643c8aeb742b02677ff059caf4

Clear openslide_read_associated_image() dest if already in error state
f19d06e93a changed the semantics of openslide_read_associated_image()
to match openslide_read_region(): "If an error occurs or has occurred,
then the memory pointed to by @p dest will be cleared".  However, the
code change didn't implement the "or has occurred" part.  Fix this.

Fixes: f19d06e93a ("Clear openslide_read_associated_image() output buffer on error")
Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

src/openslide.c
@@ -569,27 +569,32 @@ void openslide_get_associated_image_dimensions(openslide_t *osr, const char *nam
 void openslide_read_associated_image(openslide_t *osr,
 				     const char *name,
 				     uint32_t *dest) {
+  struct _openslide_associated_image *img =
+    g_hash_table_lookup(osr->associated_images, name);
+  if (!img) {
+    return;
+  }
+  size_t pixels = img->w * img->h;
+
   if (openslide_get_error(osr)) {
+    if (dest) {
+      memset(dest, 0, pixels * sizeof(uint32_t));
+    }
     return;
   }
 
-  struct _openslide_associated_image *img = g_hash_table_lookup(osr->associated_images,
-								name);
-  if (img) {
-    size_t pixels = img->w * img->h;
-    g_autofree uint32_t *tmp_buf = NULL;
-    if (!dest) {
-      // undocumented special case for testing
-      tmp_buf = g_new(uint32_t, pixels);
-    }
+  g_autofree uint32_t *tmp_buf = NULL;
+  if (!dest) {
+    // undocumented special case for testing
+    tmp_buf = g_new(uint32_t, pixels);
+  }
 
-    GError *tmp_err = NULL;
-    if (!img->ops->get_argb_data(img, dest ? dest : tmp_buf, &tmp_err)) {
-      _openslide_propagate_error(osr, tmp_err);
-      if (dest) {
-        // ensure we don't return a partial result
-        memset(dest, 0, pixels * sizeof(uint32_t));
-      }
+  GError *tmp_err = NULL;
+  if (!img->ops->get_argb_data(img, dest ? dest : tmp_buf, &tmp_err)) {
+    _openslide_propagate_error(osr, tmp_err);
+    if (dest) {
+      // ensure we don't return a partial result
+      memset(dest, 0, pixels * sizeof(uint32_t));
     }
   }
 }