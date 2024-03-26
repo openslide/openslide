59eae621a43249a6de531b31370b76294e07ae86
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2024-03-27 00:14:14 +0900

    zeiss: merge init_range_grids() into create_levels()
    Each function builds a downsample-to-level or downsample-to-grid hash
    table.  Build the table once and reuse it.  Similarly, use the
    max_downsample argument in both places rather than re-computing it in
    the second half.
    
    Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

--- src/openslide-vendor-zeiss.c
+++ src/openslide-vendor-zeiss.c
@@ -963,7 +963,9 @@ static int compare_level_downsamples(const void *a, const void *b) {
   return (la->downsample_i < lb->downsample_i) ? -1 : 1;
 }
 
-static GPtrArray *create_levels(struct czi *czi, int64_t max_downsample) {
+static GPtrArray *create_levels(openslide_t *osr, struct czi *czi,
+                                int64_t max_downsample, GError **err) {
+  // walk subblocks, create a level struct for each valid downsample
   g_autoptr(GPtrArray) levels =
     g_ptr_array_new_full(10, (GDestroyNotify) destroy_level);
   g_autoptr(GHashTable) level_hash =
@@ -984,21 +986,21 @@ static GPtrArray *create_levels(struct czi *czi, int64_t max_downsample) {
 
       g_ptr_array_add(levels, l);
       int64_t *k = g_new(int64_t, 1);
-      *k = b->downsample_i;
+      *k = l->downsample_i;
       g_hash_table_insert(level_hash, k, l);
     }
 
     l->max_tile_w = MAX(l->max_tile_w, b->tw);
     l->max_tile_h = MAX(l->max_tile_h, b->th);
   }
+  if (!levels->len) {
+    g_set_error(err, OPENSLIDE_ERROR, OPENSLIDE_ERROR_FAILED,
+                "Found no levels in slide");
+    return NULL;
+  }
   g_ptr_array_sort(levels, compare_level_downsamples);
-  return g_steal_pointer(&levels);
-}
 
-static bool init_range_grids(openslide_t *osr, struct czi *czi,
-                             GPtrArray *levels, GError **err) {
-  g_autoptr(GHashTable) grids =
-    g_hash_table_new_full(g_int64_hash, g_int64_equal, g_free, NULL);
+  // now that we know bucket sizes, create grids
   for (guint i = 0; i < levels->len; i++) {
     struct level *l = levels->pdata[i];
     // assume the largest tile dimensions are the routine ones, and smaller
@@ -1006,40 +1008,32 @@ static bool init_range_grids(openslide_t *osr, struct czi *czi,
     l->grid = _openslide_grid_create_range(osr,
                                            l->max_tile_w, l->max_tile_h,
                                            read_tile, NULL);
-    int64_t *k = g_new(int64_t, 1);
-    *k = l->downsample_i;
-    g_hash_table_insert(grids, k, l->grid);
   }
 
-  if (!levels->len) {
-    g_set_error(err, OPENSLIDE_ERROR, OPENSLIDE_ERROR_FAILED,
-                "Found no levels in slide");
-    return false;
-  }
-  struct level *last_l = levels->pdata[levels->len - 1];
-
+  // add subblocks to grids
   for (int i = 0; i < czi->nsubblk; i++) {
     struct czi_subblk *b = &czi->subblks[i];
-    if (b->downsample_i > last_l->downsample_i) {
+    if (b->downsample_i > max_downsample) {
       // subblock from a level that was omitted because not all scenes have it
       continue;
     }
     if (!validate_subblk(b, err)) {
-      return false;
+      return NULL;
     }
 
-    struct _openslide_grid *grid = g_hash_table_lookup(grids, &b->downsample_i);
-    g_assert(grid);
-    _openslide_grid_range_add_tile(grid, (double) b->x1 / b->downsample_i,
+    struct level *l = g_hash_table_lookup(level_hash, &b->downsample_i);
+    g_assert(l);
+    _openslide_grid_range_add_tile(l->grid, (double) b->x1 / b->downsample_i,
                                    (double) b->y1 / b->downsample_i,
                                    (double) b->tw, (double) b->th, b);
   }
 
+  // postprocess grids
   for (guint i = 0; i < levels->len; i++) {
     struct level *l = levels->pdata[i];
     _openslide_grid_range_finish_adding_tiles(l->grid);
   }
-  return true;
+  return g_steal_pointer(&levels);
 }
 
 /* find offset to embedded image with @name, such as Label */
@@ -1198,8 +1192,8 @@ static bool zeiss_open(openslide_t *osr, const char *filename,
     return false;
   }
 
-  g_autoptr(GPtrArray) levels = create_levels(czi, max_downsample);
-  if (!init_range_grids(osr, czi, levels, err)) {
+  g_autoptr(GPtrArray) levels = create_levels(osr, czi, max_downsample, err);
+  if (!levels) {
     return false;
   }
 
