804fd2ba759d432d03c9ed40b6fdb0800d4294d5
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2024-05-24 16:47:04 -0500

    dicom: correctly signal error upon failure to reopen file
    Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

--- src/openslide-decode-dicom.c
+++ src/openslide-decode-dicom.c
@@ -103,7 +103,7 @@ static int64_t vfs_read(DcmError **dcm_error, DcmIO *io,
   GError *tmp_err = NULL;
   if (!ensure_file(dio, &tmp_err)) {
     propagate_gerror(dcm_error, tmp_err);
-    return 0;
+    return -1;
   }
   // openslide VFS has no error return for read()
   int64_t count = _openslide_fread(dio->file, buffer, length);
