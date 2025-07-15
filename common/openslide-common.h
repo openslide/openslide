openslide/openslide

commit da13e99ebfdaef827a6d3884431198666dc6b498

common: add helper function to fail if openslide_t is in error state
Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

common/openslide-common.h
@@ -24,6 +24,7 @@
 
 #include <stdbool.h>
 #include <glib.h>
+#include <openslide.h>
 
 #ifdef OPENSLIDE_PUBLIC
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(openslide_t, openslide_close)
@@ -50,6 +51,7 @@ void common_usage(const struct common_usage_info *info) G_GNUC_NORETURN;
 // fail
 
 void common_fail(const char *fmt, ...) G_GNUC_NORETURN;
+void common_fail_on_error(openslide_t *osr, const char *fmt, ...);
 
 // fd
 

common/openslide-common-fail.c
@@ -33,3 +33,15 @@ void common_fail(const char *fmt, ...) {
   va_end(ap);
   exit(1);
 }
+
+void common_fail_on_error(openslide_t *osr, const char *fmt, ...) {
+  const char *err = openslide_get_error(osr);
+  if (err != NULL) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, ": %s\n", err);
+    va_end(ap);
+    exit(1);
+  }
+}

test/extended.c
@@ -44,12 +44,9 @@ static void test_image_fetch(openslide_t *osr,
   for (int32_t level = 0; level < openslide_get_level_count(osr); level++) {
     openslide_read_region(osr, buf, x, y, level, w, h);
   }
-
-  const char *err = openslide_get_error(osr);
-  if (err) {
-    common_fail("Read failed: %"PRId64" %"PRId64" %"PRId64" %"PRId64": %s",
-                x, y, w, h, err);
-  }
+  common_fail_on_error(osr,
+                       "Read failed: %"PRId64" %"PRId64" %"PRId64" %"PRId64,
+                       x, y, w, h);
 }
 
 #if !defined(NONATOMIC_CLOEXEC) && !defined(_WIN32)
@@ -220,10 +217,7 @@ int main(int argc, char **argv) {
   if (!osr) {
     common_fail("Couldn't open %s", path);
   }
-  const char *err = openslide_get_error(osr);
-  if (err) {
-    common_fail("Open failed: %s", err);
-  }
+  common_fail_on_error(osr, "Open failed");
   openslide_close(osr);
 
   osr = openslide_open(path);

test/profile.c
@@ -48,10 +48,7 @@ int main(int argc, char **argv) {
   if (!osr) {
     common_fail("Couldn't open %s", path);
   }
-  const char *err = openslide_get_error(osr);
-  if (err) {
-    common_fail("Open failed: %s", err);
-  }
+  common_fail_on_error(osr, "Open failed");
   if (level >= openslide_get_level_count(osr)) {
     common_fail("No such level: %d", level);
   }
@@ -99,10 +96,7 @@ int main(int argc, char **argv) {
   CALLGRIND_STOP_INSTRUMENTATION;
 #endif
 
-  err = openslide_get_error(osr);
-  if (err) {
-    common_fail("Read failed: %s", err);
-  }
+  common_fail_on_error(osr, "Read failed");
 
   return 0;
 }

commit 271038662e6c2e3117e562fe13648c2e56203210

common: drop unnecessary ifdef
We unconditionally include openslide.h now.

Fixes: da13e99ebf ("common: add helper function to fail if openslide_t is in error state")
Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

common/openslide-common.h
@@ -25,9 +25,7 @@
 #include <glib.h>
 #include <openslide.h>
 
-#ifdef OPENSLIDE_PUBLIC
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(openslide_t, openslide_close)
-#endif
 
 // cmdline
 