openslide/openslide

commit 80a059613ec3924ed3fcbc78d0a6925d39b48f7f

aperio: use main image ICC profile for thumbnail if profile names match
The thumbnail's TIFF directory doesn't have an ICC profile, but both it
and level 0's directory have properties including an ICC profile name.
If the two profile names exist and are equal, use the main image's ICC
profile for the thumbnail.

Fixes: https://github.com/openslide/openslide/issues/637
Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

src/openslide-vendor-aperio.c
@@ -349,6 +349,7 @@ static bool add_properties(openslide_t *osr, TIFF *tiff, GError **err) {
 // true does not necessarily imply an image was added
 static bool add_associated_image(openslide_t *osr,
                                  const char *name_if_available,
+                                 tdir_t *icc_dir_if_available,
                                  struct _openslide_tiffcache *tc,
                                  TIFF *tiff,
                                  GError **err) {
@@ -384,7 +385,7 @@ static bool add_associated_image(openslide_t *osr,
 
   return _openslide_tiff_add_associated_image(osr, name, tc,
                                               TIFFCurrentDirectory(tiff),
-                                              NULL, err);
+                                              icc_dir_if_available, err);
 }
 
 static void propagate_missing_tile(void *key, void *value G_GNUC_UNUSED,
@@ -421,6 +422,12 @@ static bool aperio_open(openslide_t *osr,
   g_autoptr(_openslide_tiffcache) tc = _openslide_tiffcache_create(filename);
   g_auto(_openslide_cached_tiff) ct = _openslide_tiffcache_get(tc, err);
   if (!ct.tiff) {
+    return false;
+  }
+
+  // add properties early, since we'll need them for the thumbnail ICC
+  // profile
+  if (!add_properties(osr, ct.tiff, err)) {
     return false;
   }
 
@@ -523,8 +530,27 @@ static bool aperio_open(openslide_t *osr,
       }
     } else {
       // associated image
-      const char *name = (dir == 1) ? "thumbnail" : NULL;
-      if (!add_associated_image(osr, name, tc, ct.tiff, err)) {
+      const char *name = NULL;
+      g_autofree tdir_t *icc_dir = NULL;
+      if (dir == 1) {
+        name = "thumbnail";
+
+        g_autoptr(GHashTable) thumbnail_props = read_properties(ct.tiff, err);
+        if (!thumbnail_props) {
+          g_prefix_error(err, "Reading thumbnail properties: ");
+          return false;
+        }
+        const char *main_icc_name =
+          g_hash_table_lookup(osr->properties, "aperio.ICC Profile");
+        const char *thumbnail_icc_name =
+          g_hash_table_lookup(thumbnail_props, "ICC Profile");
+        if (main_icc_name && thumbnail_icc_name &&
+            g_str_equal(main_icc_name, thumbnail_icc_name)) {
+          // use ICC profile from first directory
+          icc_dir = g_new0(tdir_t, 1);
+        }
+      }
+      if (!add_associated_image(osr, name, icc_dir, tc, ct.tiff, err)) {
 	return false;
       }
       //g_debug("associated image: %d", dir);
@@ -537,14 +563,6 @@ static bool aperio_open(openslide_t *osr,
     struct level *l = level_array->pdata[i];
     g_hash_table_foreach(l->missing_tiles, propagate_missing_tile,
                          level_array->pdata[i + 1]);
-  }
-
-  // read properties
-  if (!_openslide_tiff_set_dir(ct.tiff, 0, err)) {
-    return false;
-  }
-  if (!add_properties(osr, ct.tiff, err)) {
-    return false;
   }
 
   // get icc profile size, if present

test/cases/aperio-thumbnail-empty-imagedescription/config.yaml
@@ -1,0 +1,7 @@
+# thumbnail ImageDescription is zero-length string
+base: Aperio/CMU-1.svs
+slide: CMU-1.svs
+success: true
+vendor: aperio
+properties:
+  openslide.associated.thumbnail.icc-size: ""

test/cases/aperio-thumbnail-mismatched-icc-profile/config.yaml
@@ -1,0 +1,7 @@
+# thumbnail ICC Profile name doesn't match main image
+base: Aperio/CMU-1.svs
+slide: CMU-1.svs
+success: true
+vendor: aperio
+properties:
+  openslide.associated.thumbnail.icc-size: ""

test/cases/aperio/config.yaml
@@ -4,6 +4,7 @@ success: true
 vendor: aperio
 primary: true
 properties:
+  openslide.associated.thumbnail.icc-size: "141992"
   openslide.icc-size: "141992"
   openslide.quickhash-1: 30f1a38031fc0e21d81f9d01435ac4af848f6fe2bbf8f7768184336ee5d7e796
   openslide.vendor: aperio

commit 6d9c30241d331b4b9637c5ca084869331ff67fd2

aperio: allow thumbnails to omit ImageDescription
KFBIO SVS exports don't attach an ImageDescription to the thumbnail.
Historically we've been fine with this, and all we lose is the thumbnail
ICC profile that clearly wasn't provided anyway.

Fixes: 80a059613ec3 ("aperio: use main image ICC profile for thumbnail if profile names match")
Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

src/openslide-vendor-aperio.c
@@ -492,19 +492,17 @@ static bool aperio_open(openslide_t *osr,
       if (dir == 1) {
         name = "thumbnail";
 
-        g_autoptr(GHashTable) thumbnail_props = read_properties(ct.tiff, err);
-        if (!thumbnail_props) {
-          g_prefix_error(err, "Reading thumbnail properties: ");
-          return false;
-        }
-        const char *main_icc_name =
-          g_hash_table_lookup(osr->properties, "aperio.ICC Profile");
-        const char *thumbnail_icc_name =
-          g_hash_table_lookup(thumbnail_props, "ICC Profile");
-        if (main_icc_name && thumbnail_icc_name &&
-            g_str_equal(main_icc_name, thumbnail_icc_name)) {
-          // use ICC profile from first directory
-          icc_dir = g_new0(tdir_t, 1);
+        g_autoptr(GHashTable) thumbnail_props = read_properties(ct.tiff, NULL);
+        if (thumbnail_props) {
+          const char *main_icc_name =
+            g_hash_table_lookup(osr->properties, "aperio.ICC Profile");
+          const char *thumbnail_icc_name =
+            g_hash_table_lookup(thumbnail_props, "ICC Profile");
+          if (main_icc_name && thumbnail_icc_name &&
+              g_str_equal(main_icc_name, thumbnail_icc_name)) {
+            // use ICC profile from first directory
+            icc_dir = g_new0(tdir_t, 1);
+          }
         }
       } else {
         uint32_t subfile;

test/cases/aperio-thumbnail-no-imagedescription/config.yaml
@@ -1,0 +1,6 @@
+base: Aperio/CMU-1.svs
+slide: CMU-1.svs
+success: true
+vendor: aperio
+properties:
+  openslide.associated.thumbnail.width: '1024'